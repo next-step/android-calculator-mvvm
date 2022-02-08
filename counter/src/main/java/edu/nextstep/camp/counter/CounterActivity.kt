17c67d1ba9060787fd5926d3d1255d2aa38d6e3b
jinsu4755 <parkjinsu4755@gmail.com>
2022-02-09 04:56:05 +0900

    feat(step1): Activity Binding, ViewModel 연결
    - binding 인스턴스 생성 코드 작성
    - viewModel 필드로 작성후 생성 위임
    - binding lifecycleOwner, viewModel 연결

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -1,11 +1,19 @@
 package edu.nextstep.camp.counter
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+    private val counterViewModel: CounterViewModel by viewModels()
+    private lateinit var binding: ActivityCounterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        val binding = ActivityCounterBinding.inflate(layoutInflater)
+        binding.lifecycleOwner = this
+        binding.viewModel = counterViewModel
+        setContentView(binding.root)
     }
 }
