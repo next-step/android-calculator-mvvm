04b77498ce3b000f9bdf05ecb3afa14b3a92c2ce
jinsu4755 <parkjinsu4755@gmail.com>
2022-02-09 05:09:25 +0900

    feat(step1): Counter 0 이하 이벤트 작성
    - count 0 이하에서 Toast 메시지 보여주는 코드 작성
    - xml에서 toString() 빼먹어서 작성함
    - binding이 Activity에서 필드로 존재할 필요 없어보여서 삭제

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -1,19 +1,27 @@
 package edu.nextstep.camp.counter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
     private val counterViewModel: CounterViewModel by viewModels()
-    private lateinit var binding: ActivityCounterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityCounterBinding.inflate(layoutInflater)
-        binding.lifecycleOwner = this
-        binding.viewModel = counterViewModel
-        setContentView(binding.root)
+        ActivityCounterBinding.inflate(layoutInflater).apply {
+            lifecycleOwner = this@CounterActivity
+            viewModel = counterViewModel
+        }.also { setContentView(it.root) }
+        observeCounterViewModelEvent()
+    }
+
+    private fun observeCounterViewModelEvent() {
+        counterViewModel.lessThenZeroEvent.observe(this) {
+            Toast.makeText(this, "0 이하로 내릴 수 없습니다", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 }
