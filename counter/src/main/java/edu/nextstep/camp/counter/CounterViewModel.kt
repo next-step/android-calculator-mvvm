35637e4c197f6bdf926b673f17e7a548bc160f75
jinsu4755 <parkjinsu4755@gmail.com>
2022-02-09 04:45:52 +0900

    feat(step1): Counter 증가, 감소 로직 작성
    - count 증가 함수를 ViewModel에 작성
    - count 감소 함수를 ViewModel에 작성함
    - count 의 경우 0 이하가 불가능하기에 이를 View에 알릴 livedata Event 생성

--- counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
@@ -8,4 +8,22 @@ class CounterViewModel : ViewModel() {
     private val _count = MutableLiveData(0)
     val count: LiveData<Int>
         get() = _count
+
+    private val _lessThenZeroEvent = MutableLiveData<Event<Unit>>()
+    val lessThenZeroEvent: LiveData<Event<Unit>>
+        get() = _lessThenZeroEvent
+
+    fun increaseCount() {
+        var count = _count.value ?: return
+        _count.value = ++count
+    }
+
+    fun decreaseCount() {
+        var count = _count.value ?: return
+        if (count <= 0) {
+            _lessThenZeroEvent.value = Event(Unit)
+            return
+        }
+        _count.value = --count
+    }
 }
