f723683095e16ffe9a203910c6bcc669c58721f8
Jinsu Park <parkjinsu4755@gmail.com>
2022-02-09 23:45:39 +0900

    Step1 - Counter 구현 (#2)
    * feat(step1): CounterViewModel 작성
    
    * feat(step1): xml을 binding layout 으로 변경
    
    * feat(step1): Counter 증가, 감소 로직 작성
    
    - count 증가 함수를 ViewModel에 작성
    - count 감소 함수를 ViewModel에 작성함
    - count 의 경우 0 이하가 불가능하기에 이를 View에 알릴 livedata Event 생성
    
    * feat(step1): xml에 ViewModel 연결
    
    * feat(step1): Activity Binding, ViewModel 연결
    
    - binding 인스턴스 생성 코드 작성
    - viewModel 필드로 작성후 생성 위임
    - binding lifecycleOwner, viewModel 연결
    
    * feat(step1): Counter 0 이하 이벤트 작성
    
    - count 0 이하에서 Toast 메시지 보여주는 코드 작성
    - xml에서 toString() 빼먹어서 작성함
    - binding이 Activity에서 필드로 존재할 필요 없어보여서 삭제

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -1,11 +1,27 @@
 package edu.nextstep.camp.counter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+    private val counterViewModel: CounterViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        ActivityCounterBinding.inflate(layoutInflater).apply {
+            lifecycleOwner = this@CounterActivity
+            viewModel = counterViewModel
+        }.also { setContentView(it.root) }
+        observeCounterViewModelEvent()
+    }
+
+    private fun observeCounterViewModelEvent() {
+        counterViewModel.lessThenZeroEvent.observe(this) {
+            Toast.makeText(this, "0 이하로 내릴 수 없습니다", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 }
