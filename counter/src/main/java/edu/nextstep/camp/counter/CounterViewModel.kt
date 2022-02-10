f4da3371d40f884d9a529883076e11ad14cf46fc
jppark <jppark@fasoo.com>
2022-02-11 00:39:56 +0900

    refactor: Counter 값 객체를 이용하도록 수정

--- counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
@@ -3,29 +3,29 @@ package edu.nextstep.camp.counter
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.lang.IllegalStateException
 
 class CounterViewModel: ViewModel() {
-    private val _count = MutableLiveData(0)
-    val count: LiveData<Int>
+    private val _count: MutableLiveData<Counter> = MutableLiveData(Counter(0))
+    val count: LiveData<Counter>
         get() = _count
+    val counter: Counter get() = _count.value ?: Counter(0)
 
     private val _countIsZeroEvent = SingleLiveEvent<Unit>()
     val countIsZeroEvent: LiveData<Unit>
         get() = _countIsZeroEvent
 
     fun upCount() {
-        _count.value?.let {
-            _count.value = it + 1
-        }
+        _count.value = counter.plusCount()
     }
 
     fun downCount() {
-        if (_count.value == 0) {
-            _countIsZeroEvent.value = Unit
-            return
+        try {
+            check (counter.number > 0)
+            _count.value = counter.minusCount()
         }
-        _count.value?.let {
-            _count.value = it - 1
+        catch (e: IllegalStateException) {
+            _countIsZeroEvent.value = Unit
         }
     }
 }
