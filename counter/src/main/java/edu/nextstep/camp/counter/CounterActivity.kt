126b4c5f45072d9fae06c685b745685bbca36ec2
junpil Park <iamfeel!2@gmail.com>
2022-02-10 13:11:57 +0900

    refactor: ViewModel 의 이름 변경

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -9,15 +9,15 @@ import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCounterBinding
-    private val mainViewModel: MainViewModel by viewModels()
+    private val counterViewModel: CounterViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_counter)
         binding.lifecycleOwner = this
-        binding.viewModel = mainViewModel
+        binding.viewModel = counterViewModel
 
-        mainViewModel.countIsZeroEvent.observe(this, {
+        counterViewModel.countIsZeroEvent.observe(this, {
             Toast.makeText(this, R.string.less_then_zero_error, Toast.LENGTH_SHORT).show()
         })
     }
--- counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel: ViewModel() {
+class CounterViewModel: ViewModel() {
     private val _count = MutableLiveData(0)
     val count: LiveData<Int>
         get() = _count
