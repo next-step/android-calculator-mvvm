cd6d221fe70529a031b737ae2e122c19d32ddd73
Black_Jin <dlwls5201@naver.com>
2022-02-10 23:43:59 +0900

    Step1 Counter 구현 (#3)
    * step1 ActivityCounterBinding 연결
    
    * step1 countViewModel 구현
    - count up, down 기능 추가
    
    * step1 0 이하는 토스트 메시지 노출
    
    * step1 junit5 라이브러리 추가
    
    * step1 CounterViewModel test 코드 추가

---
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
@@ -0,0 +1,32 @@
+package edu.nextstep.camp.counter
+
+import androidx.annotation.StringRes
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel : ViewModel() {
+
+    val eventShowToast = SingleLiveEvent<Int>()
+
+    private val _count = MutableLiveData(0)
+    val count: LiveData<Int> get() = _count
+
+    fun countUp() {
+        val currentCount = _count.value ?: 0
+        _count.value = currentCount + 1
+    }
+
+    fun countDown() {
+        val currentCount = _count.value ?: 0
+        if (currentCount <= 0) {
+            showToast(R.string.can_not_lower_below_zero)
+            return
+        }
+        _count.value = currentCount - 1
+    }
+
+    private fun showToast(@StringRes stringId: Int) {
+        eventShowToast.value = stringId
+    }
+}
