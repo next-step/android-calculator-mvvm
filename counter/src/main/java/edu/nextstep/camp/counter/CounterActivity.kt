46b5a891573d640a2e570ab9e4393cd117f70dfd
devsoupe <luvyum4e@gmail.com>
2022-02-11 15:27:44 +0900

    feat : Activity에 데이터바인딩 클래스 사용

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -2,10 +2,15 @@ package edu.nextstep.camp.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCounterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_counter)
+        setContentView(binding.root)
     }
 }
