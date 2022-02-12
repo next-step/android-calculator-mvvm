bdfa70f8381ffba2fbdeaf30d766c217db2235a1
DoDoBest <av449@naver.com>
2022-02-12 15:11:08 +0900

    chore: 강의 보면서 작성

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -3,6 +3,7 @@ package edu.nextstep.camp.counter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
@@ -19,5 +20,13 @@ class CounterActivity : AppCompatActivity() {
 
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+
+        binding.viewModel.showErrorMessage.observe(this) {
+            if (it.consumed) return@observe
+            Toast.makeText(this, "0 이하로는 내릴 수 없습니다.", Toast.LENGTH_LONG).show()
+            it.consume()
+        }
+
+
     }
 }
--- counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
@@ -6,8 +6,11 @@ import androidx.lifecycle.ViewModel
 
 class CounterViewModel : ViewModel() {
     private val _count = MutableLiveData(0)
-        val count: LiveData<Int>
-            get() = _count
+    val count: LiveData<Int> = _count
+    
+    private val _showErrorMessage = MutableLiveData<Event<Unit>>()
+    val showErrorMessage: LiveData<Event<Unit>>
+        get() = _showErrorMessage
 
     fun up() {
         val count = _count.value ?: return
@@ -16,6 +19,10 @@ class CounterViewModel : ViewModel() {
 
     fun down() {
         val count = _count.value ?: return
+        if(count == 0) {
+            _showErrorMessage.value = Event(Unit)
+            return
+        }
         _count.value = count - 1
     }
 }
