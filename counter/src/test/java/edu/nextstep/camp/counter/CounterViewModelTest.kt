499fc9a01d53ed3b91c0418500f9ccfd086ad309
Woogear Kwon <woogear.kwon@huray.net>
2022-02-13 15:47:33 +0900

    [Refactor] LiveData 테스트코드 확장함수 따로 분리

--- counter/src/test/java/edu/nextstep/camp/counter/CounterViewModelTest.kt
+++ counter/src/test/java/edu/nextstep/camp/counter/CounterViewModelTest.kt
@@ -65,28 +65,3 @@ class CounterViewModelTest {
         assertThat(viewModel.countEvent.getOrAwaitValue()).isEqualTo(expected)
     }
 }
-
-fun <T> LiveData<T>.getOrAwaitValue(
-    time: Long = 2,
-    timeUnit: TimeUnit = TimeUnit.SECONDS
-): T {
-    var data: T? = null
-    val latch = CountDownLatch(1)
-    val observer = object : Observer<T> {
-        override fun onChanged(o: T?) {
-            data = o
-            latch.countDown()
-            this@getOrAwaitValue.removeObserver(this)
-        }
-    }
-
-    this.observeForever(observer)
-
-    // Don't wait indefinitely if the LiveData is not set.
-    if (!latch.await(time, timeUnit)) {
-        throw TimeoutException("LiveData value was never set.")
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    return data as T
-}
