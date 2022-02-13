b4cfa642ad77cc446d51f6be262b64e6da9a9ae1
ganghee <idiosauri@naver.com>
2022-02-13 12:38:04 +0900

    refact: singleLiveEvent 주석추가

--- counter/src/main/java/edu/nextstep/camp/counter/SingleLiveEvent.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/SingleLiveEvent.kt
@@ -9,6 +9,8 @@ import java.util.concurrent.atomic.AtomicBoolean
 class SingleLiveEvent<T>(value: T?) : MutableLiveData<T>() {
     private val pending = AtomicBoolean(false)
 
+    // 하나의 옵저빙하는 곳만 발행이 된다.
+    // set이 되었을 때만 발행이 된다. 화면 회전할 때는 get이라 다시 발행되지 않는다.
     override fun observe(owner: LifecycleOwner, observer: Observer<in T?>) {
         super.observe(owner) { value ->
             if (pending.compareAndSet(true, false)) {
