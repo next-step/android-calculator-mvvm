f7a618f02e64f13a412feb9318b5c0bd6f420c64
dlwls5201 <dlwls5201@naver.com>
2022-02-14 22:17:59 +0900

    [step2] CalculatorViewModel 적용

--- calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
@@ -2,46 +2,26 @@ package edu.nextstep.camp.calculator
 
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityCalculatorBinding
 
-class CalculatorActivity : AppCompatActivity(), CalculatorContract.View {
+class CalculatorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCalculatorBinding
-    override lateinit var presenter: CalculatorContract.Presenter
+    private val viewModel: CalculatorViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculatorBinding.inflate(layoutInflater)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
         setContentView(binding.root)
-        presenter = CalculatorPresenter(this)
-
-        binding.button0.setOnClickListener { presenter.addToExpression(0) }
-        binding.button1.setOnClickListener { presenter.addToExpression(1) }
-        binding.button2.setOnClickListener { presenter.addToExpression(2) }
-        binding.button3.setOnClickListener { presenter.addToExpression(3) }
-        binding.button4.setOnClickListener { presenter.addToExpression(4) }
-        binding.button5.setOnClickListener { presenter.addToExpression(5) }
-        binding.button6.setOnClickListener { presenter.addToExpression(6) }
-        binding.button7.setOnClickListener { presenter.addToExpression(7) }
-        binding.button8.setOnClickListener { presenter.addToExpression(8) }
-        binding.button9.setOnClickListener { presenter.addToExpression(9) }
-        binding.buttonPlus.setOnClickListener { presenter.addToExpression(Operator.Plus) }
-        binding.buttonMinus.setOnClickListener { presenter.addToExpression(Operator.Minus) }
-        binding.buttonMultiply.setOnClickListener { presenter.addToExpression(Operator.Multiply) }
-        binding.buttonDivide.setOnClickListener { presenter.addToExpression(Operator.Divide) }
-        binding.buttonDelete.setOnClickListener { presenter.removeLast() }
-        binding.buttonEquals.setOnClickListener { presenter.calculate() }
+        initViewModel()
     }
 
-    override fun showExpression(expression: Expression) {
-        binding.textView.text = expression.toString()
-    }
-
-    override fun showResult(result: Int) {
-        binding.textView.text = result.toString()
-    }
-
-    override fun showIncompleteExpressionError() {
-        Toast.makeText(this, R.string.incomplete_expression, Toast.LENGTH_SHORT).show()
+    private fun initViewModel() {
+        viewModel.eventShowIncompleteExpressionError.observe(this) {
+            Toast.makeText(this, R.string.incomplete_expression, Toast.LENGTH_SHORT).show()
+        }
     }
 }
