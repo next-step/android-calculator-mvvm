07aba6a35aae5d0c6b117c55ad9965cf41b21df1
ganghee <idiosauri@naver.com>
2022-02-14 23:56:19 +0900

    refact: DiffUtil 클래스 수정

--- calculator/src/main/java/edu/nextstep/camp/calculator/RecordAdapter.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/RecordAdapter.kt
@@ -1,7 +1,9 @@
 package edu.nextstep.camp.calculator
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import edu.nextstep.camp.calculator.databinding.ItemResultBinding
 import edu.nextstep.camp.calculator.domain.model.RecordStatement
@@ -16,4 +18,15 @@ class RecordAdapter : ListAdapter<RecordStatement, RecordViewHolder>(DiffCallbac
 
     override fun onBindViewHolder(holder: RecordViewHolder, position: Int) =
         holder.bind(getItem(position))
+
+    class DiffCallbackUtil : DiffUtil.ItemCallback<RecordStatement>() {
+        override fun areItemsTheSame(oldItem: RecordStatement, newItem: RecordStatement): Boolean =
+            oldItem.uuid == newItem.uuid
+
+        @SuppressLint("DiffUtilEquals")
+        override fun areContentsTheSame(
+            oldItem: RecordStatement,
+            newItem: RecordStatement
+        ): Boolean = oldItem == newItem
+    }
 }
