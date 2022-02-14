a53b9b4d9002d888d7b804724bde704ca0409c5b
Woody <woody.lee@balancehero.com>
2022-02-14 19:20:41 +0900

    feature: MVVM으로 Counter 구현

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -2,10 +2,27 @@ package edu.nextstep.camp.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+
+    private val counterViewModel: CounterViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        val binding = ActivityCounterBinding.inflate(layoutInflater).apply {
+            viewModel = counterViewModel
+            lifecycleOwner = this@CounterActivity
+        }
+
+        counterViewModel.eventShowErrorMessage.observe(this) {
+            if (it.consumed) return@observe
+            Toast.makeText(this, "0 이하로는 내릴 수 없습니다.", Toast.LENGTH_SHORT).show()
+            it.consume()
+        }
+
+        setContentView(binding.root)
     }
 }
---
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
@@ -0,0 +1,30 @@
+package edu.nextstep.camp.counter
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel : ViewModel() {
+
+    private val _count = MutableLiveData(0)
+    val count: LiveData<Int> get() = _count
+
+    private val _eventShowErrorMessage = MutableLiveData<Event<Unit>>()
+    val eventShowErrorMessage: LiveData<Event<Unit>> get() = _eventShowErrorMessage
+
+    fun onClickButtonUp() {
+        val count = _count.value ?: return
+        _count.value = count + 1
+    }
+
+    fun onClickButtonDown() {
+        val count = _count.value ?: return
+
+        if(count == 0) {
+            _eventShowErrorMessage.value = Event(Unit)
+            return
+        }
+
+        _count.value = count - 1
+    }
+}
