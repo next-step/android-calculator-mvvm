f359484c542ca49422beafb393fe021ec29e67d5
DoDoBest <48354989+DoTheBestMayB@users.noreply.github.com>
2022-02-14 12:40:46 +0900

    Step1 - Counter 구현 (#7)
    * chore: 강의 보면서 작성
    
    * chore: 강의 보면서 작성
    
    * fix: binding.viewModel이 아닌 CounterActivity의 viewModel에 구독 함수 설정하기

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -2,10 +2,31 @@ package edu.nextstep.camp.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCounterBinding
+    private val viewModel: CounterViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        binding = ActivityCounterBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_counter)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        viewModel.showErrorMessage.observe(this) {
+            if (it.consumed) return@observe
+            Toast.makeText(this, "0 이하로는 내릴 수 없습니다.", Toast.LENGTH_LONG).show()
+            it.consume()
+        }
+
+
     }
 }
---
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
@@ -0,0 +1,28 @@
+package edu.nextstep.camp.counter
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel : ViewModel() {
+    private val _count = MutableLiveData(0)
+    val count: LiveData<Int> = _count
+    
+    private val _showErrorMessage = MutableLiveData<Event<Unit>>()
+    val showErrorMessage: LiveData<Event<Unit>>
+        get() = _showErrorMessage
+
+    fun up() {
+        val count = _count.value ?: return
+        _count.value = count + 1
+    }
+
+    fun down() {
+        val count = _count.value ?: return
+        if(count == 0) {
+            _showErrorMessage.value = Event(Unit)
+            return
+        }
+        _count.value = count - 1
+    }
+}
