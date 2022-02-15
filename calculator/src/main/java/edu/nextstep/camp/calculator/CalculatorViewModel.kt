7318b31fa34f9aeea80bc020fe66b13ed45a6725
jinsu4755 <parkjinsu4755@gmail.com>
2022-02-16 03:36:31 +0900

    feat(step2): CalculatorViewModel 작성

---
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorViewModel.kt
@@ -0,0 +1,45 @@
+package edu.nextstep.camp.calculator
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import edu.nextstep.camp.calculator.utils.Event
+import edu.nextstep.camp.calculator.utils.NonNullLiveData
+import edu.nextstep.camp.domain.calculator.Calculator
+import edu.nextstep.camp.domain.calculator.Expression
+import edu.nextstep.camp.domain.calculator.Operator
+
+class CalculatorViewModel(
+    private val initialExpression: Expression = Expression.EMPTY
+) {
+    private val calculator = Calculator()
+
+    private val _expression = NonNullLiveData(initialExpression)
+    val expression: LiveData<Expression>
+        get() = _expression
+
+    private val _incompleteExpressionEvent = MutableLiveData<Event<Boolean>>()
+    val incompleteExpressionEvent: LiveData<Event<Boolean>>
+        get() = _incompleteExpressionEvent
+
+    fun addToExpression(operand: Int) {
+        _expression.value += operand
+    }
+
+    fun addToExpression(operator: Operator) {
+        _expression.value += operator
+    }
+
+    fun removeLast() {
+        _expression.value = _expression.value.removeLast()
+    }
+
+    fun calculate() {
+        val currentExpression = _expression.value
+        val result = calculator.calculate(currentExpression.toString())
+        if (result != null) {
+            _expression.value = Expression(result)
+            return
+        }
+        _incompleteExpressionEvent.value = Event(true)
+    }
+}
