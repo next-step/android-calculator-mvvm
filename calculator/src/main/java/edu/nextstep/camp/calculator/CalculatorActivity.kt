663b6c73ca804f5f72038244087c77e77e15e67d
Woogear Kwon <woogear.kwon@huray.net>
2022-02-15 23:37:19 +0900

    [REFACTOR] CalculatorActivity에서 MVP 패턴 관련 코드 제거

--- calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
@@ -8,12 +8,11 @@ import edu.nextstep.camp.calculator.databinding.ActivityCalculatorBinding
 import edu.nextstep.camp.domain.Expression
 import edu.nextstep.camp.domain.Operator
 
-class CalculatorActivity : AppCompatActivity(), CalculatorContract.View {
+class CalculatorActivity : AppCompatActivity() {
 
     private val viewModel: CalculatorViewModel by viewModels()
 
     private lateinit var binding: ActivityCalculatorBinding
-    override lateinit var presenter: CalculatorContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,32 +22,14 @@ class CalculatorActivity : AppCompatActivity(), CalculatorContract.View {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
-        presenter = CalculatorPresenter(this)
-
         initViews()
         observeLiveData()
     }
 
     private fun observeLiveData() {
-//        viewModel.expressionEvent.observe(this) {
-//            binding.textView.text = it.toString()
-//        }
-
         viewModel.errorEvent.observe(this) {
             Toast.makeText(this, R.string.incomplete_expression, Toast.LENGTH_SHORT).show()
         }
-    }
-
-    override fun showExpression(expression: Expression) {
-//        binding.textView.text = expression.toString()
-    }
-
-    override fun showResult(result: Int) {
-//        binding.textView.text = result.toString()
-    }
-
-    override fun showIncompleteExpressionError() {
-//        Toast.makeText(this, R.string.incomplete_expression, Toast.LENGTH_SHORT).show()
     }
 
     private fun initViews() {
