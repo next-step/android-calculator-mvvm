7a1ac73255f73f608462b24580e2f6a3a4b4983a
Woogear Kwon <woogear.kwon@huray.net>
2022-02-16 23:55:38 +0900

    [Refactor] Activity의 SetOnClickLister() 바인딩 함수로 대체

--- calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
@@ -22,7 +22,6 @@ class CalculatorActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
-        initViews()
         observeLiveData()
     }
 
@@ -30,24 +29,5 @@ class CalculatorActivity : AppCompatActivity() {
         viewModel.errorEvent.observe(this) {
             Toast.makeText(this, R.string.incomplete_expression, Toast.LENGTH_SHORT).show()
         }
-    }
-
-    private fun initViews() {
-        binding.button0.setOnClickListener { viewModel.addToExpression(0) }
-        binding.button1.setOnClickListener { viewModel.addToExpression(1) }
-        binding.button2.setOnClickListener { viewModel.addToExpression(2) }
-        binding.button3.setOnClickListener { viewModel.addToExpression(3) }
-        binding.button4.setOnClickListener { viewModel.addToExpression(4) }
-        binding.button5.setOnClickListener { viewModel.addToExpression(5) }
-        binding.button6.setOnClickListener { viewModel.addToExpression(6) }
-        binding.button7.setOnClickListener { viewModel.addToExpression(7) }
-        binding.button8.setOnClickListener { viewModel.addToExpression(8) }
-        binding.button9.setOnClickListener { viewModel.addToExpression(9) }
-        binding.buttonPlus.setOnClickListener { viewModel.addToExpression(Operator.Plus) }
-        binding.buttonMinus.setOnClickListener { viewModel.addToExpression(Operator.Minus) }
-        binding.buttonMultiply.setOnClickListener { viewModel.addToExpression(Operator.Multiply) }
-        binding.buttonDivide.setOnClickListener { viewModel.addToExpression(Operator.Divide) }
-        binding.buttonDelete.setOnClickListener { viewModel.removeLast() }
-        binding.buttonEquals.setOnClickListener { viewModel.calculate() }
     }
 }
