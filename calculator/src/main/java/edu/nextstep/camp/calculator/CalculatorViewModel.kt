6f310896ab13d00ebfb841965eddc0a99d536932
dlwls5201 <dlwls5201@naver.com>
2022-02-17 01:08:25 +0900

    [step3] Calculator Memory 기능 추가

--- calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorViewModel.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorViewModel.kt
@@ -3,8 +3,12 @@ package edu.nextstep.camp.calculator
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
-class CalculatorViewModel : ViewModel() {
+class CalculatorViewModel(
+    private val memoryDao: MemoryDao
+) : ViewModel() {
 
     private val calculator = Calculator()
 
@@ -15,6 +19,11 @@ class CalculatorViewModel : ViewModel() {
 
     private val expressionNotNull get() = _expression.value ?: Expression.EMPTY
 
+    private val _isVisibleMemoryList = MutableLiveData(false)
+    val isVisibleMemoryList: LiveData<Boolean> = _isVisibleMemoryList
+
+    val memoryFlow = memoryDao.getAllMemory()
+
     fun addToExpression(operand: Int) {
         _expression.postValue(expressionNotNull.plus(operand))
     }
@@ -24,12 +33,13 @@ class CalculatorViewModel : ViewModel() {
     }
 
     fun calculate() {
-        val result = calculator.calculate(expression.value.toString())
+        val result = calculator.calculate(expressionNotNull.toString())
         if (result == null) {
             showIncompleteExpressionError()
             return
         }
         _expression.postValue(Expression(listOf(result)))
+        saveExpressionToMemory(result)
     }
 
     fun removeLast() {
@@ -38,7 +48,20 @@ class CalculatorViewModel : ViewModel() {
         }
     }
 
+    fun showAndHideMemoryListVisible() {
+        isVisibleMemoryList.value?.let {
+            _isVisibleMemoryList.postValue(it.not())
+        }
+    }
+
     private fun showIncompleteExpressionError() {
         eventShowIncompleteExpressionError.call()
+    }
+
+    private fun saveExpressionToMemory(result: Int) {
+        viewModelScope.launch {
+            val expression = expressionNotNull.toString()
+            memoryDao.insert(Memory(expression = expression, result = result))
+        }
     }
 }
