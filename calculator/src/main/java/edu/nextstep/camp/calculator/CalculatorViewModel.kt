ea23d6c66225a38657a370b37454cf7f312afb6d
simjunbo <simuelunbo@gmail.com>
2022-02-18 01:31:13 +0900

    feature: 기존 presenter 로직 viewmodel로 변환

---
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorViewModel.kt
@@ -0,0 +1,40 @@
+package edu.nextstep.camp.calculator
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import edu.nextstep.camp.calculator.domain.Calculator
+import edu.nextstep.camp.calculator.domain.Expression
+import edu.nextstep.camp.calculator.domain.Operator
+
+class CalculatorViewModel(private val calculator: Calculator = Calculator()) : ViewModel() {
+
+    private val _expression = MutableLiveData<Expression>(Expression.EMPTY)
+    val expression: LiveData<Expression>
+        get() = _expression
+
+    private val _expressionError = MutableLiveData<Event<Unit>>()
+    val expressionError: LiveData<Event<Unit>>
+        get() = _expressionError
+
+    fun addToExpression(operand: Int) {
+        _expression.value = _expression.value?.plus(operand)
+    }
+
+    fun addToExpression(operator: Operator) {
+        _expression.value = _expression.value?.plus(operator)
+    }
+
+    fun removeLast() {
+        _expression.value = expression.value?.removeLast()
+    }
+
+    fun calculate() {
+        val result = calculator.calculate(expression.value.toString())
+        if (result == null) {
+            _expressionError.value = Event(Unit)
+        } else {
+            _expression.value = Expression(listOf(result))
+        }
+    }
+}
