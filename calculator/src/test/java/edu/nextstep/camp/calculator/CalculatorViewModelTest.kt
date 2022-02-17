d3d96ac8fcb394655efe2197a8a499b27e02b53d
jinsu4755 <parkjinsu4755@gmail.com>
2022-02-17 23:18:20 +0900

    feat(step2): 연산자로 끝나는 수식에 연산자 추가하는 경우 테스트 추가
    - 마지막이 연산자인 수식에 연산자를 추가하면 추가한 연산자로 변경되는지 확인하는 테스트 작성
    - ex) 수식 1 + 에서 - 추가하면 1 -이 된다

--- calculator/src/test/java/edu/nextstep/camp/calculator/CalculatorViewModelTest.kt
+++ calculator/src/test/java/edu/nextstep/camp/calculator/CalculatorViewModelTest.kt
@@ -55,6 +55,18 @@ class CalculatorViewModelTest {
         val actualExpression = viewModel.expression.getOrAwaitValue()
         assertThat(actualExpression.toString()).isEqualTo("1 +")
     }
+
+    @Test
+    fun `수식 1 +에서, 연산자 -가 입력되면, 수식을 1 -로 갱신한다`() {
+        // given :
+        val viewModel = CalculatorViewModel(Expression(1, Operator.Plus))
+        // when :
+        viewModel.addToExpression(Operator.Minus)
+        // then :
+        val actualExpression = viewModel.expression.getOrAwaitValue()
+        assertThat(actualExpression.toString()).isEqualTo("1 -")
+    }
+
     // 입력된 피연산자가 없을 때, 사용자가 연산자 +, -, ×, ÷ 버튼을 누르면 화면에 아무런 변화가 없어야 한다.
     // -  -> + 클릭 ->
     // 입력된 피연산자가 있을 때, 사용자가 연산자 +, -, ×, ÷ 버튼을 누르면 해당 기호가 화면에 보여야 한다.
