7c317b7452513e1ec5db916bfa2515f4142c9ef3
jinsu4755 <parkjinsu4755@gmail.com>
2022-02-17 23:26:35 +0900

    feat(step2): 빈 수식에서 마지막 제거하는 경우 테스트 추가
    - 빈 수식에서 마지막 요소를 제거하는 경우 수식 변경이 없음을 확인하는 테스트 작성

--- calculator/src/test/java/edu/nextstep/camp/calculator/CalculatorViewModelTest.kt
+++ calculator/src/test/java/edu/nextstep/camp/calculator/CalculatorViewModelTest.kt
@@ -67,13 +67,17 @@ class CalculatorViewModelTest {
         assertThat(actualExpression.toString()).isEqualTo("1 -")
     }
 
-    // 입력된 피연산자가 없을 때, 사용자가 연산자 +, -, ×, ÷ 버튼을 누르면 화면에 아무런 변화가 없어야 한다.
-    // -  -> + 클릭 ->
-    // 입력된 피연산자가 있을 때, 사용자가 연산자 +, -, ×, ÷ 버튼을 누르면 해당 기호가 화면에 보여야 한다.
-    // - 1 -> + 클릭 -> 1 +
-    // - 1 + -> - 클릭 -> 1 -
-    // 입력된 수식이 없을 때, 사용자가 지우기 버튼을 누르면 화면에 아무런 변화가 없어야 한다.
-    // -  -> 지우기 클릭 ->
+    @Test
+    fun `빈 수식에서, 마지막을 제거하면, 수식이 변경되지 않는다`() {
+        // given :
+        val viewModel = CalculatorViewModel(initialExpression = Expression.EMPTY)
+        // when :
+        viewModel.removeLast()
+        // then :
+        val actualExpression = viewModel.expression.getOrAwaitValue()
+        assertThat(actualExpression.toString()).isEmpty()
+    }
+
     // 입력된 수식이 있을 때, 사용자가 지우기 버튼을 누르면 수식에 마지막으로 입력된 연산자 또는 피연산자가 지워져야 한다.
     // - 32 + 1 -> 지우기 클릭 -> 32 + -> 지우기 클릭 -> 32 -> 지우기 클릭 -> 3 -> 지우기 클릭 ->  -> 지우기 클릭 ->
     // 입력된 수신이 완전할 때, 사용자가 = 버튼을 누르면 입력된 수식의 결과가 화면에 보여야 한다.
