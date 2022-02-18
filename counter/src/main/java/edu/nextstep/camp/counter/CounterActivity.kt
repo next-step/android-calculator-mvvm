67556a4e3e95c9a055529b1dffed6505970c5737
younghwan <show2881@gmail.com>
2022-02-18 23:11:17 +0900

    [feature] CounterViewModel 추가 (#22)
    Co-authored-by: yhwanshin <yhwanshin@smilegate.com>

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -2,10 +2,29 @@ package edu.nextstep.camp.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCounterBinding
+
+    private val counterViewModel: CounterViewModel by viewModels { ViewModelFactory(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        binding = ActivityCounterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        binding.lifecycleOwner = this
+        binding.viewModel = counterViewModel
+        counterViewModel.showToast.observe(this) {
+            Toast.makeText(this, R.string.error_counter_under_zero, Toast.LENGTH_SHORT).show()
+        }
     }
 }
