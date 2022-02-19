162def71b0f0a5f3dc86eb2ce58c9e5c6da65e87
ganghee <idiosauri@naver.com>
2022-02-19 23:31:33 +0900

    refact: RepoInjector 인터페이스 구현

--- data/src/main/java/edu/nextstep/camp/calculator/data/di/RepoInjector.kt
+++ data/src/main/java/edu/nextstep/camp/calculator/data/di/RepoInjector.kt
@@ -1,13 +1,10 @@
 package edu.nextstep.camp.calculator.data.di
 
 import android.content.Context
-import edu.nextstep.camp.calculator.data.CalculatorRepositoryImpl
-import edu.nextstep.camp.calculator.data.local.CalculatorDatabase
 import edu.nextstep.camp.calculator.domain.CalculatorRepository
 
 object RepoInjector {
     fun provideCalculatorRepository(context: Context): CalculatorRepository {
-        val database = CalculatorDatabase.getDatabase(context)
-        return CalculatorRepositoryImpl.getInstance(database.calculatorDao())
+        return RepoInjectorHelper().provideCalculatorRepository(context)
     }
 }
