2d83772d2c41db8fc5e14dfc1c2fc6db4d5836c1
LeeChungWoo <skcjddn@gmail.com>
2022-02-20 20:17:58 +0900

    feature: databinding,viewmodel 로직 추가

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -2,10 +2,24 @@ package edu.nextstep.camp.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCounterBinding
+    private val viewModel: CounterViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_counter)
+
+       viewModel.showErrorMessage.observe(this){
+            Toast.makeText(this,"0 이하로는 내릴 수 없습니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
     }
 }
