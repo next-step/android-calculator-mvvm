3b15ca3b957b44571fd6b937622b37ce4d961ccf
ganghee <idiosauri@naver.com>
2022-02-20 11:50:48 +0900

    refact: RepoInjector 수정

--- data/src/main/java/edu/nextstep/camp/calculator/data/di/RepoInjector.kt
+++ data/src/main/java/edu/nextstep/camp/calculator/data/di/RepoInjector.kt
@@ -1,10 +1,13 @@
 package edu.nextstep.camp.calculator.data.di
 
 import android.content.Context
+import edu.nextstep.camp.calculator.data.CalculatorRepositoryImpl
+import edu.nextstep.camp.calculator.data.local.CalculatorDatabase
 import edu.nextstep.camp.calculator.domain.CalculatorRepository
 
 object RepoInjector {
     fun provideCalculatorRepository(context: Context): CalculatorRepository {
-        return RepoInjectorHelper().provideCalculatorRepository(context)
+        val database = CalculatorDatabase.getDatabase(context)
+        return CalculatorRepositoryImpl.getInstance(database.calculatorDao())
     }
 }
