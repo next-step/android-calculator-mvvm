032e54eabb504cf6827d462f3efddfb48bce3ce6
jinsu4755 <parkjinsu4755@gmail.com>
2022-02-22 20:43:14 +0900

    feat(step3): 계산 기록 어뎁터 작성

---
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorMemoryAdapter.kt
@@ -0,0 +1,48 @@
+package edu.nextstep.camp.calculator
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import edu.nextstep.camp.calculator.databinding.ItemResultBinding
+
+class CalculatorMemoryAdapter :
+    ListAdapter<CalculatorRecordUiState, CalculatorMemoryAdapter.RecordViewHolder>(
+        RecordDiffUtilCallback()
+    ) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemResultBinding.inflate(layoutInflater, parent, false)
+        return RecordViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holderRecord: RecordViewHolder, position: Int) {
+        holderRecord.onBind(getItem(position))
+    }
+
+    class RecordViewHolder(
+        private val binding: ItemResultBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(item: CalculatorRecordUiState) {
+            binding.uiState = item
+        }
+    }
+
+    private class RecordDiffUtilCallback : DiffUtil.ItemCallback<CalculatorRecordUiState>() {
+        override fun areItemsTheSame(
+            oldItem: CalculatorRecordUiState,
+            newItem: CalculatorRecordUiState
+        ): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(
+            oldItem: CalculatorRecordUiState,
+            newItem: CalculatorRecordUiState
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
