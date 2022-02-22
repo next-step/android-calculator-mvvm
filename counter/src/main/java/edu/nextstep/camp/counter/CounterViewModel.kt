677dc72aa1c171d0f1defcf40725a868f9161861
LeeChungWoo92 <skcjddn@gmail.com>
2022-02-22 13:23:53 +0900

    1단계 - Counter 구현 (#28)
    * refactor: Databinding convert
    
    * add: CounterViewModel 생성
    
    * feature: CounterViewModel 로직 작성
    
    * feature: databinding 로직 추가
    
    * feature: databinding,viewmodel 로직 추가
    
    * refactor: 빌드오류로 인한 버전업

---
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
@@ -0,0 +1,27 @@
+package edu.nextstep.camp.counter
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel : ViewModel() {
+    private val _count = MutableLiveData(0)
+    val count: LiveData<Int> = _count
+
+    private val _showErrorMessage = MutableLiveData<Unit>()
+    val showErrorMessage: LiveData<Unit> = _showErrorMessage
+
+    fun countUp() {
+        val count = _count.value ?: return
+        _count.value = count + 1
+    }
+
+    fun countDown() {
+        val count = _count.value ?: return
+        if (count == 0) {
+            _showErrorMessage.value = Unit
+            return
+        }
+        _count.value = count - 1
+    }
+}
