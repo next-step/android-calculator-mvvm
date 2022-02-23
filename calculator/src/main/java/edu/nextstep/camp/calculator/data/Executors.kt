ef802d36990a25e4227a57efc978a6b74246b321
DoDoBest <av449@naver.com>
2022-02-23 15:37:27 +0900

    refactor: 사용하지 않는 코루틴 관련 클래스 삭제

--- calculator/src/main/java/edu/nextstep/camp/calculator/data/Executors.kt
+++
@@ -1,9 +0,0 @@
-package edu.nextstep.camp.calculator.data
-
-import java.util.concurrent.Executors
-
-private val IO_EXECUTOR = Executors.newSingleThreadExecutor()
-
-fun ioThread(f : () -> Unit) {
-    IO_EXECUTOR.execute(f)
-}
