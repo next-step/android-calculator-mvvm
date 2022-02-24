e18a03b8292c7561060f46fd92af4400a5db4a7e
jinsu4755 <parkjinsu4755@gmail.com>
2022-02-24 22:52:38 +0900

    feat(step3): 계산기록 local DB 접근 DAO 작성
    - 모든 기록의 수식과 결과를 가져오는 함수 작성

---
+++ data/src/main/java/edu/nextstep/camp/data/CalculatorRecordDAO.kt
@@ -0,0 +1,11 @@
+package edu.nextstep.camp.data
+
+import androidx.room.Dao
+import androidx.room.Query
+import edu.nextstep.camp.domain.calculator.CalculatorMemory
+
+@Dao
+internal interface CalculatorRecordDAO {
+    @Query("SELECT expression, result FROM CalculatorRecord")
+    fun getAllRecord(): List<CalculatorMemory.Record>
+}
