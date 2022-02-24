d5f6d487fe426ed3259eb8cb02998d6d223a43b5
Woogear Kwon <woogear.kwon@huray.net>
2022-02-24 23:15:09 +0900

    [Refactor] 어댑터에서 뷰홀더 분리
    - 피드백 반영

--- calculator/src/main/java/edu/nextstep/camp/calculator/ui/MemoryAdapter.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/ui/MemoryAdapter.kt
@@ -2,15 +2,12 @@ package edu.nextstep.camp.calculator.ui
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import edu.nextstep.camp.calculator.R
-import edu.nextstep.camp.calculator.databinding.ItemResultBinding
 import edu.nextstep.camp.data.Memory
 
-class MemoryAdapter : RecyclerView.Adapter<MemoryAdapter.ViewHolder>() {
+class MemoryAdapter : RecyclerView.Adapter<MemoryViewHolder>() {
     private val memories: MutableList<Memory> = mutableListOf()
 
     @SuppressLint("NotifyDataSetChanged")
@@ -22,20 +19,14 @@ class MemoryAdapter : RecyclerView.Adapter<MemoryAdapter.ViewHolder>() {
 
     override fun getItemCount(): Int = memories.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemoryViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_result, parent, false)
-        return ViewHolder(view)
+        return MemoryViewHolder(view)
     }
 
     @SuppressLint("SetTextI18n")
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MemoryViewHolder, position: Int) {
         holder.tvExpression.text = memories[position].expression
         holder.tvResult.text = "= ${memories[position].result}"
-    }
-
-    inner class ViewHolder(containerView: View) : RecyclerView.ViewHolder(containerView) {
-        private val binding = ItemResultBinding.bind(containerView)
-        val tvExpression: TextView = binding.tvExpression
-        val tvResult: TextView = binding.tvResult
     }
 }
