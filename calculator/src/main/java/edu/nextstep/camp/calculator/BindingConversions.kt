be221a7cf943b7208909a274a0294a2dd90205f9
junpilpark <iamfeel2@gmail.com>
2022-02-25 23:36:22 +0900

    refactor: BindingAdapters 사용하지 않고 xml 의 onClick 이용하도록 수정

--- calculator/src/main/java/edu/nextstep/camp/calculator/BindingConversions.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/BindingConversions.kt
@@ -2,21 +2,6 @@ package edu.nextstep.camp.calculator
 
 import androidx.databinding.BindingConversion
 import edu.nextstep.camp.calculator.domain.Expression
-import edu.nextstep.camp.calculator.domain.Operator
-
-@BindingConversion
-fun convertStringToInt(operand: String): Int = operand.toInt()
-
-@BindingConversion
-fun convertStringToOperator(operator: String): Operator? {
-    val operatorStr =
-        when (operator) {
-            "÷" -> "/"
-            "×" -> "*"
-            else -> operator
-        }
-    return Operator.of(operatorStr)
-}
 
 @BindingConversion
 fun convertExpressionToString(expression: Expression) = expression.toString()
