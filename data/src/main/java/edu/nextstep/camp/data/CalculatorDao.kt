a09ae090cda4505dec183045fd8a9a6df3290a1c
LeeChungWoo <skcjddn@gmail.com>
2022-03-01 14:19:53 +0900

    add: room calculator dao 추가

---
+++ data/src/main/java/edu/nextstep/camp/data/CalculatorDao.kt
@@ -0,0 +1,15 @@
+package edu.nextstep.camp.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface CalculatorDao {
+
+    @Query("SELECT * FROM calculatorHisotry")
+    fun getCalculatorHisotry(): List<CalculatorHisotry>
+
+    @Insert
+    fun inserCalculatorHisotry(calculatorHisotry: CalculatorHisotry)
+}
