166538c4cebdc9a5c5800be0dcd015832bf4ca3e
LeeChungWoo <skcjddn@gmail.com>
2022-03-02 17:50:16 +0900

    refactor : ViewModelFactory 및 Adapter추가

--- calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
@@ -1,14 +1,19 @@
 package edu.nextstep.camp.calculator
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import edu.nextstep.camp.calculator.databinding.ActivityCalculatorBinding
+import edu.nextstep.camp.data.CalculatorDatabase
 
 class CalculatorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCalculatorBinding
-    private val viewModel: CalculatorViewModel by viewModels()
+    private val viewModel: CalculatorViewModel by viewModels { ViewModelFactory(this) }
+    private val historyAdapter = CalculatorHistoryAdapter()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +23,35 @@ class CalculatorActivity : AppCompatActivity() {
 
         binding.lifecycleOwner = this
         binding.vm = viewModel
+        binding.recyclerView.adapter = historyAdapter
 
 
         viewModel.showErrorMessage.observe(this) {
             it.consume()?.let {
                 Toast.makeText(this, R.string.incomplete_expression, Toast.LENGTH_SHORT).show()
             }
+        }
+
+        viewModel.historyList.observe(this) {
+            historyAdapter.submitList(it)
+
+        }
+
+    }
+
+    class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return when (modelClass) {
+                CalculatorViewModel::class.java -> createMainViewModel()
+                else -> throw IllegalArgumentException()
+            } as T
+        }
+
+        private fun createMainViewModel(): CalculatorViewModel {
+            return CalculatorViewModel(
+                calculatorDao = CalculatorDatabase.getInstance(context).calculatorDao()
+            )
         }
     }
 
