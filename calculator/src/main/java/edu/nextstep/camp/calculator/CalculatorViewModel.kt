49f8eeec429647b3cf9142064e4ce00b16803305
LeeChungWoo <skcjddn@gmail.com>
2022-03-02 17:49:40 +0900

    refactor : room관련 코드 추가

--- calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorViewModel.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorViewModel.kt
@@ -6,8 +6,14 @@ import androidx.lifecycle.ViewModel
 import edu.nextstep.camp.calculator.domain.Calculator
 import edu.nextstep.camp.calculator.domain.Expression
 import edu.nextstep.camp.calculator.domain.Operator
+import edu.nextstep.camp.data.CalculatorDao
+import edu.nextstep.camp.data.CalculatorHisotry
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class CalculatorViewModel : ViewModel() {
+class CalculatorViewModel(private val calculatorDao: CalculatorDao) :
+    ViewModel() {
     private val calculator = Calculator()
     private var expression = Expression.EMPTY
 
@@ -17,6 +23,13 @@ class CalculatorViewModel : ViewModel() {
 
     private val _showErrorMessage = MutableLiveData<Event<Unit>>()
     val showErrorMessage: LiveData<Event<Unit>> = _showErrorMessage
+
+    private val _isVisibleHistoryList = MutableLiveData(false)
+    val isVisibleHistoryList: LiveData<Boolean> = _isVisibleHistoryList
+
+    private val _historyList = MutableLiveData<List<CalculatorHisotry>>(emptyList())
+    val historyList: LiveData<List<CalculatorHisotry>> = _historyList
+
 
     fun addToExpression(operand: Int) {
         expression += operand
@@ -40,7 +53,21 @@ class CalculatorViewModel : ViewModel() {
             return
         }
         _text.value = result.toString()
+
+        val newHistory = CalculatorHisotry(expression.toString(), result.toString())
+        CoroutineScope(Dispatchers.IO).launch {
+            calculatorDao.insertCalculatorHisotry(newHistory)
+        }
     }
 
+    fun showOrHideHistoryList() {
+        _isVisibleHistoryList.value = _isVisibleHistoryList.value != true
+
+        if (_isVisibleHistoryList.value == false) {
+            CoroutineScope(Dispatchers.IO).launch {
+                _historyList.postValue(calculatorDao.getCalculatorHisotry())
+            }
+        }
+    }
 
 }
