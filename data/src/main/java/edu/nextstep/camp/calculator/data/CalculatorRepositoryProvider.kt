7ae938479847a17da6738cf720fec77f5ba141a3
junpilpark <iamfeel2@gmail.com>
2022-03-03 10:47:45 +0900

    refactor: object 로 변경

--- data/src/main/java/edu/nextstep/camp/calculator/data/CalculatorRepositoryProvider.kt
+++ data/src/main/java/edu/nextstep/camp/calculator/data/CalculatorRepositoryProvider.kt
@@ -3,17 +3,15 @@ package edu.nextstep.camp.calculator.data
 import android.content.Context
 import edu.nextstep.camp.calculator.data.local.CalculatorDatabase
 
-class CalculatorRepositoryProvider {
-    companion object {
-        private var instance: CalculateRepository? = null
+object CalculatorRepositoryProvider {
+    private var instance: CalculateRepository? = null
 
-        fun getInstance(context: Context): CalculateRepository {
-            return instance ?: buildCalculateStorage(context).also { instance = it }
-        }
+    fun getInstance(context: Context): CalculateRepository {
+        return instance ?: buildCalculateStorage(context).also { instance = it }
+    }
 
-        private fun buildCalculateStorage(context: Context): CalculateRepository {
-            val historyDao = CalculatorDatabase.getInstance(context).getHistoryDao()
-            return LocalCalculateRepository(historyDao)
-        }
+    private fun buildCalculateStorage(context: Context): CalculateRepository {
+        val historyDao = CalculatorDatabase.getInstance(context).getHistoryDao()
+        return LocalCalculateRepository(historyDao)
     }
 }
