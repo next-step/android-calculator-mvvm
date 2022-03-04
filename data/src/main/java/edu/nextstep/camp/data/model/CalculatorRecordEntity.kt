4056a80a544e0e6abd71f74510d66313bac3a1f5
jinsu4755 <parkjinsu4755@gmail.com>
2022-03-05 03:37:53 +0900

    feat(step3): data Entity to domain model 확장 함수 작성

--- data/src/main/java/edu/nextstep/camp/data/model/CalculatorRecordEntity.kt
+++ data/src/main/java/edu/nextstep/camp/data/model/CalculatorRecordEntity.kt
@@ -2,6 +2,7 @@ package edu.nextstep.camp.data.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import edu.nextstep.camp.domain.calculator.model.CalculatorRecord
 
 @Entity
 data class CalculatorRecordEntity(
@@ -10,3 +11,7 @@ data class CalculatorRecordEntity(
     val expression: String,
     val result: String
 )
+
+fun List<CalculatorRecordEntity>.toListOfDomain() = this.map { it.toDomain() }
+
+fun CalculatorRecordEntity.toDomain() = CalculatorRecord(this.expression, this.result)
