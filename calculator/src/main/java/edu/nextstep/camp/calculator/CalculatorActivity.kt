23111f3726b17751f90589bc828a944235d1ed49
junpilpark <iamfeel2@gmail.com>
2022-03-04 11:14:47 +0900

    test: Hisotry를 표시하는 버튼을 클릭하면 저장되어 있는_History가 표시 형식에 맞게 표시되는 지 확인하는 테스트 추가

--- calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
@@ -3,24 +3,28 @@ package edu.nextstep.camp.calculator
 import android.app.AlertDialog
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import edu.nextstep.camp.calculator.data.CalculatorRepositoryProvider
 import edu.nextstep.camp.calculator.data.local.History
 import edu.nextstep.camp.calculator.databinding.ActivityCalculatorBinding
 
 class CalculatorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCalculatorBinding
-    private val viewModel: CalculatorViewModel by viewModels {
-        CalculatorViewModelFactory(calculateRepository = CalculatorRepositoryProvider.getInstance(applicationContext))
-    }
+    private lateinit var  viewModel: CalculatorViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_calculator)
         binding.lifecycleOwner = this
-        binding.viewModel = viewModel
 
+        viewModel = createViewModel()
+        binding.viewModel = viewModel
+        initViewModelObserves(viewModel)
+    }
+
+    private fun initViewModelObserves(viewModel: CalculatorViewModel) {
         viewModel.calculateFailed.observe(this) {
             Toast.makeText(this, R.string.incomplete_expression, Toast.LENGTH_SHORT).show()
         }
@@ -28,11 +32,23 @@ class CalculatorActivity : AppCompatActivity() {
         viewModel.calculateHistory.observe(this) {
             it?.let {
                 AlertDialog.Builder(this)
-                    .setMessage(it.joinToString(separator = "\n\n") { history ->  getStringForDisplay(history) })
+                    .setMessage(it.joinToString(separator = "\n\n") { history ->
+                        getStringForDisplay(history)
+                    })
                     .show()
             }
         }
     }
+
+    private fun createViewModel(): CalculatorViewModel {
+        return ViewModelProvider(
+            this,
+            CalculatorViewModelFactory(
+                calculateRepository = CalculatorRepositoryProvider.getInstance(applicationContext)
+            )
+        ).get(CalculatorViewModel::class.java)
+    }
+
     private fun getStringForDisplay(history: History): String {
         return "${history.formula}\n= ${history.calculateResult}"
     }
