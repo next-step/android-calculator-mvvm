f6635bd365690427f1a8b5bcf49e60db5a19fffa
Woogear Kwon <woogear.kwon@huray.net>
2022-03-05 12:39:43 +0900

    [Refactor] 확장함수로 구현한 바인딩어댑터 일반 함수로 수정

--- calculator/src/main/java/edu/nextstep/camp/calculator/ViewAdapter.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/ViewAdapter.kt
@@ -7,21 +7,21 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 
 @BindingAdapter(value = ["setEnabledByViewType"])
-fun Button.setEnabledByViewType(viewType: CalculatorViewType?) {
-    this.isEnabled = if (viewType == null) true else viewType is ExpressionView
+fun setEnabledByViewType(button: Button, viewType: CalculatorViewType?) {
+    button.isEnabled = if (viewType == null) true else viewType is ExpressionView
 }
 
 @BindingAdapter(value = ["setVisibilityByViewType"])
-fun TextView.setVisibilityByViewType(viewType: CalculatorViewType?) {
-    this.visibility = viewType?.let {
+fun setVisibilityByViewType(textView: TextView, viewType: CalculatorViewType?) {
+    textView.visibility = viewType?.let {
         if (it is ExpressionView) View.VISIBLE
         else View.INVISIBLE
     } ?: View.VISIBLE
 }
 
 @BindingAdapter(value = ["setVisibilityByViewType"])
-fun RecyclerView.setVisibilityByViewType(viewType: CalculatorViewType?) {
-    this.visibility = viewType?.let {
+fun setVisibilityByViewType(recyclerView: RecyclerView, viewType: CalculatorViewType?) {
+    recyclerView.visibility = viewType?.let {
         if (it is ExpressionView) View.INVISIBLE
         else View.VISIBLE
     } ?: View.INVISIBLE
