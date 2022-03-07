9e79221c9c3908aa85f17d642df905372fbb4d9a
DoDoBest <av449@naver.com>
2022-03-07 09:59:24 +0900

    test: AppDatabaseTest.kt androidTest 폴더로 이동

--- data/src/androidTest/java/data/AppDatabaseTest.kt
+++ data/src/androidTest/java/data/AppDatabaseTest.kt
@@ -1,9 +1,12 @@
-package com.github.dodobest.data
+package data
 
 import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
+import com.github.dodobest.data.AppDatabase
+import com.github.dodobest.data.ResultRecordDao
+import com.github.dodobest.data.ResultRecordEntity
 import com.github.dodobest.data.util.TestUtil
 import com.google.common.truth.Truth.assertThat
 import org.junit.After
@@ -35,17 +38,18 @@ class AppDatabaseTest {
     }
 
     @Test
-    fun `데이터를 넣으면 저장된다`() {
-        // given : "3+3", "6"을 가진 ResultRecord가 있을때
-        val expression = "3+3"
-        val result = "6"
+    fun givenResultRecord_whenInsertIntoDB_thenSaveResultRecord() {
+        // given : "2+2", "4"을 가진 ResultRecord가 있을때
+        val expression = "2+2"
+        val result = "4"
         val resultRecord: ResultRecordEntity = TestUtil.createEmptyResultRecord().copy(expression, result)
 
         // when : DB에 넣으면
         resultRecordDao.insertResultRecord(resultRecord)
 
-        // then : "3+3", "6"을 가진 ResultRecord가 저장되어야 한다
+        // then : "2+2", "4"을 가진 ResultRecord가 저장되어야 한다
         val byExpression = resultRecordDao.findResultRecordByExpression(expression)
+        resultRecord.id = byExpression.id
         assertThat(byExpression).isEqualTo(resultRecord)
     }
 }
