d72279cdda5d31f7716ccf49494a50c00dd2eeb4
LeeChungWoo <skcjddn@gmail.com>
2022-03-08 18:02:58 +0900

    refactor : domain 모듈에있는 data class로 변경

--- calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorHistoryAdapter.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorHistoryAdapter.kt
@@ -6,9 +6,9 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import edu.nextstep.camp.calculator.databinding.ItemResultBinding
-import edu.nextstep.camp.data.CalculatorHistory
+import edu.nextstep.camp.domain.CalculatorHistoryData
 
-class CalculatorHistoryAdapter : ListAdapter<CalculatorHistory, CalculatorHistoryAdapter.HistoryViewHolder>(diffUtil) {
+class CalculatorHistoryAdapter : ListAdapter<CalculatorHistoryData, CalculatorHistoryAdapter.HistoryViewHolder>(diffUtil) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryViewHolder {
         val binding = ItemResultBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -16,7 +16,7 @@ class CalculatorHistoryAdapter : ListAdapter<CalculatorHistory, CalculatorHistor
     }
 
     override fun onBindViewHolder(holder: HistoryViewHolder, position: Int) {
-            val data = getItem(position) as CalculatorHistory
+            val data = getItem(position) as CalculatorHistoryData
             holder.bind(data)
 
     }
@@ -25,7 +25,7 @@ class CalculatorHistoryAdapter : ListAdapter<CalculatorHistory, CalculatorHistor
     class HistoryViewHolder(private val binding: ItemResultBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(calculatorHistoryList: CalculatorHistory) {
+        fun bind(calculatorHistoryList: CalculatorHistoryData) {
             binding.tvExpression.text = calculatorHistoryList.expression
             binding.tvResult.text = calculatorHistoryList.result
         }
@@ -33,11 +33,11 @@ class CalculatorHistoryAdapter : ListAdapter<CalculatorHistory, CalculatorHistor
     }
 
     companion object {
-        val diffUtil = object : DiffUtil.ItemCallback<CalculatorHistory>() {
-            override fun areItemsTheSame(oldItem: CalculatorHistory, newItem: CalculatorHistory) =
+        val diffUtil = object : DiffUtil.ItemCallback<CalculatorHistoryData>() {
+            override fun areItemsTheSame(oldItem: CalculatorHistoryData, newItem: CalculatorHistoryData) =
                 oldItem.hashCode() == newItem.hashCode()
 
-            override fun areContentsTheSame(oldItem: CalculatorHistory, newItem: CalculatorHistory) =
+            override fun areContentsTheSame(oldItem: CalculatorHistoryData, newItem: CalculatorHistoryData) =
                 oldItem == newItem
 
 
