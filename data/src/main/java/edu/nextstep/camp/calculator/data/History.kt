a7764fb4175e9784361f8cd27c6c2bf0e604ba2d
simjunbo <simuelunbo@gmail.com>
2022-03-13 06:25:49 +0900

    refactor: data 모듈 클래스 접근 제어자 internal로 변경

--- data/src/main/java/edu/nextstep/camp/calculator/data/History.kt
+++ data/src/main/java/edu/nextstep/camp/calculator/data/History.kt
@@ -2,11 +2,19 @@ package edu.nextstep.camp.calculator.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import edu.nextstep.camp.calculator.domain.model.Memory
 
 @Entity
-data class History(
+internal data class History(
     val expression: String,
     val resultValue: String
 ) {
     @PrimaryKey(autoGenerate = true) var id: Int = 0
+
+    fun convertHistoryToMemory() : Memory {
+        return Memory(
+            expression = this.expression,
+            resultValue = this.resultValue,
+        )
+    }
 }
