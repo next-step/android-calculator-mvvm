6f182779d55cd0f79191e9c8e7dca1d4433c27e9
simjunbo <simuelunbo@gmail.com>
2022-03-13 20:02:36 +0900

    feature: Injector database, dao 추가

--- data/src/main/java/edu/nextstep/camp/calculator/data/di/Injector.kt
+++ data/src/main/java/edu/nextstep/camp/calculator/data/di/Injector.kt
@@ -1,12 +1,21 @@
 package edu.nextstep.camp.calculator.data.di
 
 import android.content.Context
+import edu.nextstep.camp.calculator.data.HistoryDao
 import edu.nextstep.camp.calculator.data.HistoryDatabase
 import edu.nextstep.camp.calculator.data.HistoryRepositoryImpl
 import edu.nextstep.camp.calculator.domain.HistoryRepository
 
 object Injector {
     fun provideMemoryRepository(context: Context): HistoryRepository {
-        return HistoryRepositoryImpl(HistoryDatabase.getInstance(context).historyDao())
+        return HistoryRepositoryImpl(provideHistoryDao(provideHistoryDatabase(context)))
+    }
+
+    private fun provideHistoryDatabase(context: Context): HistoryDatabase {
+        return HistoryDatabase.getInstance(context)
+    }
+
+    private fun provideHistoryDao(database: HistoryDatabase): HistoryDao {
+        return database.historyDao()
     }
 }
