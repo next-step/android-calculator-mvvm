5c11c3dd2b86098d824b8e4b4e16f54808132c62
simjunbo <simuelunbo@gmail.com>
2022-03-13 19:55:26 +0900

    refactor: fakeRepository 생성자로 초기 값으로 받게 변경

--- calculator/src/test/java/edu/nextstep/camp/calculator/FakeRepository.kt
+++ calculator/src/test/java/edu/nextstep/camp/calculator/FakeRepository.kt
@@ -1,13 +1,16 @@
 package edu.nextstep.camp.calculator
 
-import androidx.annotation.VisibleForTesting
 import edu.nextstep.camp.calculator.domain.HistoryRepository
 import edu.nextstep.camp.calculator.domain.model.Memory
-import java.util.LinkedHashMap
 
-class FakeRepository : HistoryRepository{
-
+class FakeRepository(vararg memories: Memory) : HistoryRepository {
     var memoriesServiceData: LinkedHashMap<String, Memory> = LinkedHashMap()
+
+    init {
+        for (memory in memories) {
+            memoriesServiceData[memory.expression] = memory
+        }
+    }
 
     override suspend fun getAll(): List<Memory> {
         return memoriesServiceData.values.toList()
@@ -17,10 +20,4 @@ class FakeRepository : HistoryRepository{
         memoriesServiceData[memory.expression] = memory
     }
 
-    @VisibleForTesting
-    fun addMemory(vararg memories: Memory) {
-        for (memory in memories) {
-            memoriesServiceData[memory.expression] = memory
-        }
-    }
 }
