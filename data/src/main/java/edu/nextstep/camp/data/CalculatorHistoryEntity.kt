21da89473a1b0232f602c57f417c2a3c6527645c
LeeChungWoo <skcjddn@gmail.com>
2022-03-15 18:32:50 +0900

    refactor : internal 추가 및 toDomain 확장함수 제거

--- data/src/main/java/edu/nextstep/camp/data/CalculatorHistoryEntity.kt
+++ data/src/main/java/edu/nextstep/camp/data/CalculatorHistoryEntity.kt
@@ -5,14 +5,10 @@ import androidx.room.PrimaryKey
 import edu.nextstep.camp.domain.CalculatorHistory
 
 @Entity
-data class CalculatorHistoryEntity(
+internal data class CalculatorHistoryEntity(
     val expression: String,
     val result: String,
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
 )
 
-fun CalculatorHistoryEntity.toDomain(): CalculatorHistory= CalculatorHistory(
-    this.expression,
-    this.result
-)
