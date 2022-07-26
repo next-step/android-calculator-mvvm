46a6f7cb3089f46cef47e02d70b379f03d6eeb19
seungmin shin <greedy0110@gmail.com>
2022-07-27 03:09:55 +0900

    🚀 step1 (#53)
    * DataBinding 적용
    
    * step1 구현 완

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -1,11 +1,31 @@
 package edu.nextstep.camp.counter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import edu.nextstep.camp.counter.CounterViewModel.ViewEvent
+import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityCounterBinding.inflate(layoutInflater) }
+    private val viewModel by viewModels<CounterViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        setContentView(binding.root)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        viewModel.event.observe(this) { event ->
+            event.consume()?.let { handleViewEvent(it) }
+        }
+    }
+
+    private fun handleViewEvent(viewEvent: ViewEvent) {
+        when (viewEvent) {
+            is ViewEvent.FailDecrement ->
+                Toast.makeText(this, viewEvent.message, Toast.LENGTH_SHORT).show()
+        }
     }
 }
