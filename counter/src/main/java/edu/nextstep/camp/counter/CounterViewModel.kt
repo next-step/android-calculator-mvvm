d0db059b0c3f8d53657998389662b6a6f8c7e3e1
Suhyeon Kim <wisemuji@gmail.com>
2022-07-27 03:57:40 +0900

    feat: UP 버튼을 클릭하면 숫자가 1 증가해야 한다

---
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
@@ -0,0 +1,20 @@
+package edu.nextstep.camp.counter
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel : ViewModel() {
+
+    private val _count = MutableLiveData(0)
+    val count: LiveData<Int>
+        get() = _count
+
+    fun increase() {
+        _count.value = _count.value?.plus(COUNT_UNIT)
+    }
+
+    companion object {
+        private const val COUNT_UNIT = 1
+    }
+}
---
+++ counter/src/test/java/edu/nextstep/camp/counter/CounterViewModelTest.kt
@@ -0,0 +1,26 @@
+package edu.nextstep.camp.counter
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import org.junit.Rule
+import org.junit.Test
+
+internal class CounterViewModelTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private val viewModel: CounterViewModel = CounterViewModel()
+
+    @Test
+    fun `UP 버튼을 클릭하면 숫자가 1 증가해야 한다`() {
+        // given
+        val givenCount = viewModel.count.getOrAwaitValue()
+
+        // when
+        viewModel.increase()
+
+        // then
+        assertThat(viewModel.count.getOrAwaitValue()).isEqualTo(givenCount + 1)
+    }
+}
