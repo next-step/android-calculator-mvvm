56fb07f6155ae6397fe29d46c3e9677af48f9543
Ben <ben.to@kakaocorp.com>
2022-07-27 19:54:53 +0900

    Step1(feat) : Counter 구현 - UP 버튼을 클릭하면 숫자가 1 증가해야 한다. - DOWN 버튼을 클릭하면 숫자가 1 감소해야 한다. - 숫자는 음수가 될 수 없다. - 0일 때, DOWN 버튼을 클릭하면 0 이하로 내릴 수 없습니다 토스트 메시지가 보여야 한다.

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -2,10 +2,29 @@ package edu.nextstep.camp.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+    private val viewModel: CounterViewModel by viewModels()
+    private lateinit var binding: ActivityCounterBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        binding = ActivityCounterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            lifecycleOwner = this@CounterActivity
+            vm = viewModel
+        }
+
+        viewModel.sideEffect.observe(this) { event ->
+            if (event.consume() == CounterViewModel.SideEffect.NegativeCounter) {
+                Toast.makeText(this, getString(R.string.negative_number_toast), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
