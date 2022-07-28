e3cf2dccf4c987368902988fe01a20bc78d185d5
sinoso <tpgus123123@naver.com>
2022-07-29 07:05:34 +0900

    feat : CounterActivity viewModel 연결

--- counter/src/main/java/edu/nextstep/camp/counter/CounterEvent.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterEvent.kt
@@ -2,6 +2,6 @@ package edu.nextstep.camp.counter
 
 import androidx.annotation.StringRes
 
-enum class CounterEvent(@StringRes val id: Int) {
+enum class CounterEvent(@StringRes val stringId: Int) {
     CAN_NOT_DECREASE_COUNT_UNDER_0(R.string.counter_event_can_not_decrease_count_under_0)
 }
