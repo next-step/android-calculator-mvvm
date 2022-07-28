0545999453e66950cb6757b4cdd6879c2e0e04f2
SeHyeon Kim <tpgus123123@naver.com>
2022-07-29 07:40:20 +0900

    Step1 - Counter 구현 (#68)
    * feat : CountViewModel 구현
    
    * feat : CounterActivity viewModel 연결
    
    * refactor : countEvent 전달시 Event class 사용으로 교체

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -2,10 +2,34 @@ package edu.nextstep.camp.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+    private val viewModel: CounterViewModel by viewModels()
+    private lateinit var binding: ActivityCounterBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        binding = ActivityCounterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setBindingSetting()
+        observeViewModel()
     }
+
+    private fun setBindingSetting() {
+        binding.lifecycleOwner = this@CounterActivity
+        binding.viewModel = viewModel
+    }
+
+    private fun observeViewModel() {
+        viewModel.countEvent.observe(this) {
+            it.consume()?.let(::showToastWithCounterEvent)
+        }
+    }
+
+    private fun showToastWithCounterEvent(event: CounterEvent) =
+        Toast.makeText(this, event.stringId, Toast.LENGTH_SHORT).show()
 }
---
+++ counter/src/test/java/edu/nextstep/camp/counter/CounterViewModelTest.kt
@@ -0,0 +1,74 @@
+package edu.nextstep.camp.counter
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import edu.nextstep.camp.counter.CounterEvent.*
+import org.junit.Rule
+import org.junit.Test
+
+class CounterViewModelTest {
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+    lateinit var viewModel: CounterViewModel
+
+    @Test
+    fun `count값이 2일때 증가 요청시 count값이 1 증가 한 값인 3이 된다`() {
+        // given count 값이 2 일때
+        val startValue = 2
+        viewModel = CounterViewModel(startValue)
+
+        // when 증가 요청시
+        viewModel.increaseCount()
+
+        // then count 값은 3이 된다.
+        val expected = 3
+        val actual = viewModel.count.getOrAwaitValue()
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `count값이 3일때 감소 요청시 count값이 1 감소 한 값인 2이 된다`() {
+        // given count 값이 3 일때
+        val startValue = 3
+        viewModel = CounterViewModel(startValue)
+
+        // when 감소 요청시
+        viewModel.decreaseCount()
+
+        // then count 값은 2이 된다.
+        val expected = 2
+        val actual = viewModel.count.getOrAwaitValue()
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `count값이 0일때 감소 요청시 count값은 0을 유지한다`() {
+        // given count 값이 0 일때
+        val startValue = 0
+        viewModel = CounterViewModel(startValue)
+
+        // when 감소 요청시
+        viewModel.decreaseCount()
+
+        // then count 값은 0을 유지한다.
+        val expected = 0
+        val actual = viewModel.count.getOrAwaitValue()
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `count값이 0일때 감소 요청시 0미만으로 내릴 수 없음 event를 전달 한다`() {
+        // given count 값이 0 일때
+        val startValue = 0
+        viewModel = CounterViewModel(startValue)
+
+        // when 감소 요청시
+        viewModel.decreaseCount()
+
+        // then 0미만으로 내릴 수 없음 event를 전달 한다
+        val expected = CAN_NOT_DECREASE_COUNT_UNDER_0
+        val actual = viewModel.countEvent.getOrAwaitValue().consume()
+        assertThat(actual).isEqualTo(expected)
+    }
+
+}
