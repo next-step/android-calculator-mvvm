67927158c6e8b2d95b1f0fdb45d4bd0fb97290ef
hyowan <woga1999@gmail.com>
2022-07-29 22:56:19 +0900

    fix: import, 로직, test code 함수명 및 vm 함수명 개선

--- calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorViewModel.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorViewModel.kt
@@ -1,45 +1,44 @@
 package edu.nextstep.camp.calculator
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import edu.nextstep.camp.domain.Calculator
 import edu.nextstep.camp.domain.Expression
 import edu.nextstep.camp.domain.Operator
 
 class CalculatorViewModel : ViewModel() {
-    private val calculator = edu.nextstep.camp.domain.Calculator()
-    private var expression = edu.nextstep.camp.domain.Expression.EMPTY
+    private val calculator = Calculator()
+    var expression = Expression.EMPTY
 
-    private val _result = SingleLiveEvent<String>()
-    val result: LiveData<String>
+    private val _result = MutableLiveData(expression)
+    val result: LiveData<Expression>
         get() = _result
 
     private val _error = SingleLiveEvent<CalculatorErrorEvent>()
     val error: LiveData<CalculatorErrorEvent>
         get() = _error
 
-    fun clickNumberButton(operand: Int) {
-        expression += operand
-        _result.value = expression.toString()
+    fun addOperandToExpression(operand: Int) {
+        _result.value = _result.value?.plus(operand)
     }
 
-    fun clickOperatorButton(operator: edu.nextstep.camp.domain.Operator) {
-        expression += operator
-        _result.value = expression.toString()
+    fun addOperatorToExpression(operator: Operator) {
+        _result.value = _result.value?.plus(operator)
     }
 
-    fun clickDeleteButton() {
+    fun deleteExpression() {
         expression = expression.removeLast()
-        _result.value = expression.toString()
+        _result.value = expression
     }
 
-    fun clickEqualButton() {
+    fun calculateExpression() {
         val result = calculator.calculate(expression.toString())
         if (result == null) {
             _error.value = CalculatorErrorEvent.IncompleteExpressionError
         } else {
-            expression = edu.nextstep.camp.domain.Expression(listOf(result))
-            _result.value = expression.toString()
+            expression = Expression(listOf(result))
+            _result.value = expression
         }
     }
 }
--- calculator/src/test/java/edu/nextstep/camp/calculator/CalculatorViewModelTest.kt
+++ calculator/src/test/java/edu/nextstep/camp/calculator/CalculatorViewModelTest.kt
@@ -2,6 +2,7 @@ package edu.nextstep.camp.calculator
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.google.common.truth.Truth.assertThat
+import edu.nextstep.camp.domain.Expression
 import edu.nextstep.camp.domain.Operator
 import org.junit.Rule
 
@@ -23,8 +24,8 @@ class CalculatorViewModelTest {
     @Test
     fun `입력된_피연사자가 없을때 사용자가 피연산자 0 버튼을 누르면 화면에 해당 숫자가 보여야 한다`() {
         //when
-        viewModel.clickNumberButton(0)
-        val actual = viewModel.result.value
+        viewModel.addOperandToExpression(0)
+        val actual = viewModel.result.value.toString()
         //then
         assertThat(actual).isEqualTo("0")
     }
@@ -32,11 +33,11 @@ class CalculatorViewModelTest {
     @Test
     fun `입력된 피연산자 8이 입력되어 있을 때 9를 입력하면 89가 보여야 한다`() {
         //given
-        viewModel.clickNumberButton(8)
+        viewModel.addOperandToExpression(8)
 
         //when
-        viewModel.clickNumberButton(9)
-        val actual = viewModel.result.value
+        viewModel.addOperandToExpression(9)
+        val actual = viewModel.result.value.toString()
 
         //then
         assertThat(actual).isEqualTo("89")
@@ -45,8 +46,8 @@ class CalculatorViewModelTest {
     @Test
     fun `입력된 피연산자가 없을 때 사용자가 연산자 +를 누르면 화면에 아무런 변화가 없어야 한다`() {
         //when
-        viewModel.clickOperatorButton(Operator.Plus)
-        val actual = viewModel.result.value
+        viewModel.addOperatorToExpression(Operator.Plus)
+        val actual = viewModel.result.value.toString()
 
         //then
         assertThat(actual).isEqualTo("")
@@ -55,12 +56,12 @@ class CalculatorViewModelTest {
     @Test
     fun `1에서 연산자 +를 클릭하고 사용자가 연산자 -를 누르면 해당 기호로 변해야 한다`() {
         //given
-        viewModel.clickNumberButton(1)
-        viewModel.clickOperatorButton(Operator.Plus)
+        viewModel.addOperandToExpression(1)
+        viewModel.addOperatorToExpression(Operator.Plus)
 
         //when
-        viewModel.clickOperatorButton(Operator.Minus)
-        val actual = viewModel.result.value
+        viewModel.addOperatorToExpression(Operator.Minus)
+        val actual = viewModel.result.value.toString()
 
         //then
         assertThat(actual).isEqualTo("1 -")
@@ -69,8 +70,8 @@ class CalculatorViewModelTest {
     @Test
     fun `입력된 수식이 없을때, 사용자가 지우기 버튼을 누르면 아무런 변화가 없어야 한다`() {
         //when
-        viewModel.clickDeleteButton()
-        val actual = viewModel.result.value
+        viewModel.deleteExpression()
+        val actual = viewModel.result.value.toString()
 
         //then
         assertThat(actual).isEqualTo("")
@@ -79,15 +80,13 @@ class CalculatorViewModelTest {
     @Test
     fun `32 + 1 이라는 수식이 있을 때, 사용자가 지우기 버튼을 2번 연속 누르면 마지막으로 입력된 피연산자 1과 연산자 +가 지워져야 한다`() {
         //given
-        viewModel.clickNumberButton(3)
-        viewModel.clickNumberButton(2)
-        viewModel.clickOperatorButton(Operator.Plus)
-        viewModel.clickNumberButton(1)
+        val expression = Expression(listOf(32,Operator.Plus,1))
+        viewModel.expression = expression
 
         //when
-        viewModel.clickDeleteButton()
-        viewModel.clickDeleteButton()
-        val actual = viewModel.result.value
+        viewModel.deleteExpression()
+        viewModel.deleteExpression()
+        val actual = viewModel.result.value.toString()
 
         //then
         assertThat(actual).isEqualTo("32")
@@ -96,26 +95,25 @@ class CalculatorViewModelTest {
     @Test
     fun `3 + 2 라는 완전한 수식이 있을 때 = 버튼을 누르면 계산 결과가 도출된다`() {
         //given
-        viewModel.clickNumberButton(3)
-        viewModel.clickOperatorButton(Operator.Plus)
-        viewModel.clickNumberButton(2)
+        val expression = Expression(listOf(3, Operator.Plus, 2))
+        viewModel.expression = expression
 
         //when
-        viewModel.clickEqualButton()
-        val actual = viewModel.result.value
+        viewModel.calculateExpression()
+        val actual = viewModel.result.value.toString()
 
         //then
         assertThat(actual).isEqualTo("5")
     }
 
     @Test
-    fun `3 + 라는 불완전한 수식일 때, =을 누르면 완성되지 않은 수식이라고 토스트 메세지가 떠야 한다`() {
+    fun `3 + 라는 불완전한 수식일 때, =을 누르면 완성되지 않은 수식 error을 발생한다`() {
         //given
-        viewModel.clickNumberButton(3)
-        viewModel.clickOperatorButton(Operator.Plus)
+        viewModel.addOperandToExpression(3)
+        viewModel.addOperatorToExpression(Operator.Plus)
 
         //when
-        viewModel.clickEqualButton()
+        viewModel.calculateExpression()
         val actual = viewModel.error.value
 
         //then
