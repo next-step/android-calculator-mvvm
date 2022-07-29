e24c8df962e3a820f88b6535c5ce3c6e303fc628
AnKyungMoo <sodp2@naver.com>
2022-07-30 02:43:14 +0900

    refactor: databinding을 이용하여 onClick 관련 코드 정리

--- calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
@@ -16,23 +16,7 @@ class CalculatorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculatorBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.button0.setOnClickListener { viewModel.addToExpression(0) }
-        binding.button1.setOnClickListener { viewModel.addToExpression(1) }
-        binding.button2.setOnClickListener { viewModel.addToExpression(2) }
-        binding.button3.setOnClickListener { viewModel.addToExpression(3) }
-        binding.button4.setOnClickListener { viewModel.addToExpression(4) }
-        binding.button5.setOnClickListener { viewModel.addToExpression(5) }
-        binding.button6.setOnClickListener { viewModel.addToExpression(6) }
-        binding.button7.setOnClickListener { viewModel.addToExpression(7) }
-        binding.button8.setOnClickListener { viewModel.addToExpression(8) }
-        binding.button9.setOnClickListener { viewModel.addToExpression(9) }
-        binding.buttonPlus.setOnClickListener { viewModel.addToExpression(Operator.Plus) }
-        binding.buttonMinus.setOnClickListener { viewModel.addToExpression(Operator.Minus) }
-        binding.buttonMultiply.setOnClickListener { viewModel.addToExpression(Operator.Multiply) }
-        binding.buttonDivide.setOnClickListener { viewModel.addToExpression(Operator.Divide) }
-        binding.buttonDelete.setOnClickListener { viewModel.removeLast() }
-        binding.buttonEquals.setOnClickListener { viewModel.calculate() }
+        binding.viewModel = viewModel
     }
 
     fun showExpression(expression: Expression) {
