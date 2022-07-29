1ff71cf7dc6e75cd480bfa3c58f206b588b00d29
hyowan <woga1999@gmail.com>
2022-07-29 23:18:17 +0900

    fix: counter vm 테스트 함수 네이밍 추가 변경

--- counter/src/test/java/edu/nextstep/camp/counter/CounterViewModelTest.kt
+++ counter/src/test/java/edu/nextstep/camp/counter/CounterViewModelTest.kt
@@ -20,7 +20,7 @@ class CounterViewModelTest {
     }
 
     @Test
-    fun `0에서 up button을 누르면 count가 1로 올라간다`() {
+    fun `현재 값이 0일 경우 값을 증가시키면 1이 된다`() {
         //when
         viewModel.increment()
         val actual = viewModel.count.value
@@ -30,7 +30,7 @@ class CounterViewModelTest {
     }
 
     @Test
-    fun `2에서 down button을 누르면 count가 1로 내려간다`() {
+    fun `현재 값이 2일 경우 값을 감소시키면 1이 된다`() {
         //given
         viewModel.increment()
         viewModel.increment()
@@ -44,7 +44,7 @@ class CounterViewModelTest {
     }
 
     @Test
-    fun `0에서 down button을 눌렀을 때, count가 음수가 되면, 내려가지 않는다`() {
+    fun `감소하고자 하는 값이 음수가 될 경우 변경되지 않는다`() {
         //when
         val original = viewModel.count.value
         viewModel.decrement()
@@ -55,7 +55,7 @@ class CounterViewModelTest {
     }
 
     @Test
-    fun `0에서 down button을 눌렀을 때, count가 음수가 되면, 메세지를 업데이트 한다`() {
+    fun `감소하고자 하는 값이 음수가 되면 변경되지 않고 메세지를 업데이트 한다`() {
         //given
         val ERROR_MESSAGE = "0 이하로 내릴 수 없습니다."
 
