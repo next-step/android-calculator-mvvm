fc1b47000664eaaacb3690cb21c69d735b821582
sinoso <tpgus123123@naver.com>
2022-07-31 20:50:33 +0900

    refactor : MVP -> MVVM 리팩토링 진행

--- calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorEvent.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorEvent.kt
@@ -2,6 +2,6 @@ package edu.nextstep.camp.calculator
 
 import androidx.annotation.StringRes
 
-enum class CalculatorEvent(@StringRes stringId: Int) {
+enum class CalculatorEvent(@StringRes val stringId: Int) {
     ERROR_INCOMPLETE_EXPRESSION(R.string.incomplete_expression)
 }
