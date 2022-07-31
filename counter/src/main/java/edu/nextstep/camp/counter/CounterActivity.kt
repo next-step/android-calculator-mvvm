8f5659f627136b421eb5349ee1cacbfc84cb8c84
Choiyounho <maxosa78@gmail.com>
2022-08-01 01:49:26 +0900

    feat : 액티비티 기능 구현

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -2,10 +2,25 @@ package edu.nextstep.camp.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityCounterBinding.inflate(layoutInflater) }
+    private val counterViewModel by viewModels<CounterViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        setContentView(binding.root)
+
+        binding.counterViewModel = counterViewModel
+        binding.lifecycleOwner = this
+
+        counterViewModel.failInfo.observe(this) {
+            Toast.makeText(applicationContext, "0 미만으로 내려갈 수 없습니다.", Toast.LENGTH_LONG).show()
+        }
     }
+
 }
