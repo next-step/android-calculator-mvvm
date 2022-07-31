797b4656d6efdaed2380a4cb33a378e429038799
link-js <link.js@kakaocorp.com>
2022-07-31 22:27:16 +0900

    feat : 테스트 추가

--- calculator/src/test/java/edu/nextstep/camp/calculator/CalculatorViewModelTest.kt
+++ calculator/src/test/java/edu/nextstep/camp/calculator/CalculatorViewModelTest.kt
@@ -2,7 +2,9 @@ package edu.nextstep.camp.calculator
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.google.common.truth.Truth.assertThat
+import edu.nextstep.camp.calculator.data.History
 import edu.nextstep.camp.calculator.domain.Operator
+import io.mockk.mockk
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,7 +21,7 @@ class CalculatorViewModelTest {
 
     @Test
     fun `숫자가 입력되면 수식에 추가되고 변경된 수식을 보여줘야 한다`() {
-        viewModel = CalculatorViewModel()
+        viewModel = CalculatorViewModel(mockk())
 
         // when : 숫자 2 입력
         viewModel.addToExpression(2)
@@ -33,7 +35,7 @@ class CalculatorViewModelTest {
     fun `연산자가 입력되면 수식에 추가되고 변경된 수식을 보여줘야 한다`() {
         // given : 숫자 2
         val testExpression = listOf(2)
-        viewModel = CalculatorViewModel(initExpression = testExpression)
+        viewModel = CalculatorViewModel(mockk(), initExpression = testExpression)
 
         // when : 연산자 + 를 입력
         viewModel.addToExpression(Operator.Plus)
@@ -47,7 +49,7 @@ class CalculatorViewModelTest {
     fun `지우기가 실행되면 수식의 마지막이 지워지고 변경된 수식을 보여줘야 한다`() {
         // given : 2 +
         val testExpression = listOf(2, Operator.Plus)
-        viewModel = CalculatorViewModel(initExpression = testExpression)
+        viewModel = CalculatorViewModel(mockk(), initExpression = testExpression)
 
         // when : 지우기를 누르면
         viewModel.removeLast()
@@ -61,7 +63,7 @@ class CalculatorViewModelTest {
     fun `계산이 실행되면 계산기에 의해 계산되고 결과를 화면에 보여줘야 한다`() {
         // given : 2 + 2
         val testExpression = listOf(2, Operator.Plus, 2)
-        viewModel = CalculatorViewModel(initExpression = testExpression)
+        viewModel = CalculatorViewModel(mockk(), initExpression = testExpression)
 
         // when : 계산버튼을 누르면
         viewModel.calculate()
@@ -75,7 +77,7 @@ class CalculatorViewModelTest {
     fun `계산식이 정확하지 않을때 계산버튼을 누르면 토스트를 보낸다`() {
         // given : 정확하지 않은 계산식 입력
         val testExpression = listOf(2, Operator.Plus)
-        viewModel = CalculatorViewModel(initExpression = testExpression)
+        viewModel = CalculatorViewModel(mockk(), initExpression = testExpression)
 
         // when : 계산버튼을 누르면
         viewModel.calculate()
@@ -83,5 +85,46 @@ class CalculatorViewModelTest {
         // then : 토스트를 보낸다.
         val actual = viewModel.calculateErrorEvent.value?.consume()
         assertThat(actual).isInstanceOf(CalculatorViewModel.CalculateError.ExpressionError::class.java)
+    }
+
+    @Test
+    fun `계산기록이 안보일때 계산기록 버튼 클릭시 레이아웃이 보인다`() {
+        // given : 계산기록이 안보일때
+        viewModel = CalculatorViewModel(mockk())
+
+        // when : 계산버튼을 누르면
+        viewModel.setVisibilityHistoryLayout(true)
+
+        // then : 레이아웃이 보인다.
+        val actual = viewModel.isVisibleHistoryLayout.value
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun `계산기록이 보일때 계산기록 버튼 클릭시 레이아웃이 안보인다`() {
+        // given : 계산기록이 보일때
+        viewModel = CalculatorViewModel(mockk())
+        viewModel.setVisibilityHistoryLayout(true)
+
+        // when : 계산버튼을 누르면
+        viewModel.setVisibilityHistoryLayout(false)
+
+        // then : 레이아웃이 안보인다.
+        val actual = viewModel.isVisibleHistoryLayout.value
+        assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun `계산버튼 클릭시 계산기록이 저장된다`() {
+        // given : 2 + 2
+        val testExpression = listOf(2, Operator.Plus, 2)
+        viewModel = CalculatorViewModel(mockk(), initExpression = testExpression)
+
+        // when : 계산버튼을 누르면
+        viewModel.calculate()
+
+        // then : 계산기록이 저장된다.
+        val actual = viewModel.historyList.value
+        assertThat(actual).isEqualTo(listOf(History(expression = "2 + 2", result = 4)))
     }
 }
