a17950ba48f63361a23e5e3bf3171adb387ce009
Suhyeon Kim <wisemuji@gmail.com>
2022-08-02 02:13:36 +0900

    feat: 계산이 실행되면 기록을 저장해야 한다

--- calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorViewModel.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorViewModel.kt
@@ -3,16 +3,22 @@ package edu.nextstep.camp.calculator
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nextstep.camp.calculator.data.Record
 import com.nextstep.camp.calculator.data.RecordsRepository
 import edu.nextstep.camp.calculator.domain.Calculator
 import edu.nextstep.camp.calculator.domain.Operand
 import edu.nextstep.camp.calculator.domain.Operator
 import edu.nextstep.camp.calculator.domain.StringCalculator
 import edu.nextstep.camp.calculator.domain.StringExpressionState
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class CalculatorViewModel(
     val recordsRepository: RecordsRepository,
     val calculator: Calculator = StringCalculator,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
     state: StringExpressionState = StringExpressionState.EmptyState()
 ) : ViewModel() {
 
@@ -46,11 +52,17 @@ class CalculatorViewModel(
         }
             .onSuccess {
                 _state.value = StringExpressionState.of(it)
+                insertRecord(state = state, result = it)
             }
             .onFailure {
                 setCalculationFailed()
             }
     }
+
+    private fun insertRecord(state: StringExpressionState, result: Operand) =
+        viewModelScope.launch(dispatcher) {
+            recordsRepository.insert(Record(expression = state.toString(), result = result.value))
+        }
 
     private fun setCalculationFailed() {
         _calculationFailed.value = true
--- calculator/src/test/java/edu/nextstep/camp/calculator/CalculatorViewModelTest.kt
+++ calculator/src/test/java/edu/nextstep/camp/calculator/CalculatorViewModelTest.kt
@@ -1,9 +1,18 @@
 package edu.nextstep.camp.calculator
 
 import com.google.common.truth.Truth.assertThat
+import com.nextstep.camp.calculator.data.Record
+import com.nextstep.camp.calculator.data.RecordsRepository
 import edu.nextstep.camp.calculator.domain.Operand
 import edu.nextstep.camp.calculator.domain.Operator
 import edu.nextstep.camp.calculator.domain.StringExpressionState
+import io.mockk.coVerify
+import io.mockk.confirmVerified
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
 
@@ -14,11 +23,25 @@ internal class CalculatorViewModelTest {
     val instantTaskExecutorExtension = InstantTaskExecutorExtension()
 
     private lateinit var calculatorViewModel: CalculatorViewModel
+    private lateinit var recordsRepository: RecordsRepository
+
+    @BeforeEach
+    fun setUp() {
+        recordsRepository = mockk(relaxed = true)
+    }
+
+    @AfterEach
+    fun clearUp() {
+        confirmVerified(recordsRepository)
+    }
 
     @Test
     fun `숫자가 입력되면 수식에 추가되고 변경된 수식을 보여줘야 한다`() {
         // given
-        calculatorViewModel = CalculatorViewModel(state = StringExpressionState.EmptyState())
+        calculatorViewModel = CalculatorViewModel(
+            recordsRepository = recordsRepository,
+            state = StringExpressionState.EmptyState(),
+        )
 
         // when
         calculatorViewModel.addElement(Operand(10))
@@ -32,7 +55,10 @@ internal class CalculatorViewModelTest {
     @Test
     fun `피연산자가 입력되어 있을때, 연산자가 입력되면 수식에 추가되고 변경된 수식을 보여줘야 한다`() {
         // given
-        calculatorViewModel = CalculatorViewModel(state = StringExpressionState.of("1"))
+        calculatorViewModel = CalculatorViewModel(
+            recordsRepository = recordsRepository,
+            state = StringExpressionState.of("1")
+        )
 
         // when
         calculatorViewModel.addElement(Operator.PLUS)
@@ -46,7 +72,10 @@ internal class CalculatorViewModelTest {
     @Test
     fun `지우기가 실행되면 수식의 마지막이 지워지고 변경된 수식을 보여줘야 한다`() {
         // given
-        calculatorViewModel = CalculatorViewModel(state = StringExpressionState.of("1"))
+        calculatorViewModel = CalculatorViewModel(
+            recordsRepository = recordsRepository,
+            state = StringExpressionState.of("1")
+        )
 
         // when
         calculatorViewModel.removeElement()
@@ -57,10 +86,16 @@ internal class CalculatorViewModelTest {
         )
     }
 
+    @ExperimentalCoroutinesApi
     @Test
-    fun `계산이 실행되면 계산기에 의해 계산되고 결과를 화면에 보여줘야 한다`() {
+    fun `계산이 실행되면 계산기에 의해 계산된 결과를 화면에 보여주고 기록을 저장해야 한다`() {
         // given
-        calculatorViewModel = CalculatorViewModel(state = StringExpressionState.of("1 + 2"))
+        val givenExpression = "1 + 2"
+        calculatorViewModel = CalculatorViewModel(
+            recordsRepository = recordsRepository,
+            state = StringExpressionState.of(givenExpression),
+            dispatcher = UnconfinedTestDispatcher()
+        )
 
         // when
         calculatorViewModel.calculate()
@@ -69,12 +104,16 @@ internal class CalculatorViewModelTest {
         assertThat(calculatorViewModel.state.getOrAwaitValue()).isEqualTo(
             StringExpressionState.of("3")
         )
+        coVerify { recordsRepository.insert(Record(expression = givenExpression, result = 3.0)) }
     }
 
     @Test
     fun `완전하지 않은 형식의 계산이 실행되면 실패 토스트 메시지를 보여줘야 한다`() {
         // given
-        calculatorViewModel = CalculatorViewModel(state = StringExpressionState.of("1 +"))
+        calculatorViewModel = CalculatorViewModel(
+            recordsRepository = recordsRepository,
+            state = StringExpressionState.of("1 +")
+        )
 
         // when
         calculatorViewModel.calculate()
