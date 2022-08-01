fb2bc1a75cc71c7798cae4f660101d3f239133f1
noah.choung <noah.choung@kakaocorp.com>
2022-08-01 15:12:34 +0900

    Adapter관련 클래스 분리

--- calculator/src/main/java/edu/nextstep/camp/calculator/CalculationMemoryAdapter.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculationMemoryAdapter.kt
@@ -1,9 +1,7 @@
 package edu.nextstep.camp.calculator
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import edu.nextstep.camp.calculator.data.CalculationRecord
@@ -22,37 +20,9 @@ class CalculationMemoryAdapter() :
         return Holder(itemResultBinding)
     }
 
-
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         if (holder is Holder) {
             holder.setData(getItem(holder.adapterPosition))
-        }
-    }
-
-    class Holder(
-        private val binding: ItemResultBinding,
-    ) : RecyclerView.ViewHolder(binding.root) {
-
-        fun setData(data: CalculationRecord) {
-            binding.tvExpression.text = data.expression
-            binding.tvResult.text = "= ${data.result}"
-        }
-    }
-
-    class ItemDiffCallback : DiffUtil.ItemCallback<CalculationRecord>() {
-        override fun areItemsTheSame(
-            oldItem: CalculationRecord,
-            newItem: CalculationRecord
-        ): Boolean {
-            return oldItem.expression == newItem.expression
-        }
-
-        @SuppressLint("DiffUtilEquals")
-        override fun areContentsTheSame(
-            oldItem: CalculationRecord,
-            newItem: CalculationRecord
-        ): Boolean {
-            return oldItem == newItem
         }
     }
 }
