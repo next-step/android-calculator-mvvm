dabe9c2c00b3f09206fc534577f5c0e4e22c2be3
Lee-ji-soo <nbsp3@naver.com>
2022-08-01 18:25:39 +0900

    create Factory

---
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterViewModelFactory.kt
@@ -0,0 +1,19 @@
+package edu.nextstep.camp.counter
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class CounterViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when (modelClass) {
+            CounterViewModel::class.java -> createCounterViewModel()
+            else -> throw IllegalArgumentException()
+        } as T
+    }
+
+    private fun createCounterViewModel(): CounterViewModel {
+        return CounterViewModel(initialValue = 0)
+    }
+}
