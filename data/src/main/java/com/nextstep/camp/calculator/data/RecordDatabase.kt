0f9575e588ced749c28fb5ab3b43ea8971de6b6f
Suhyeon Kim <wisemuji@gmail.com>
2022-08-02 01:42:00 +0900

    feat: RecordDatabase 추가 및 싱글톤 인스턴스 받아올 수 있도록 구현

---
+++ data/src/main/java/com/nextstep/camp/calculator/data/RecordDatabase.kt
@@ -0,0 +1,30 @@
+package com.nextstep.camp.calculator.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Record::class], version = 1)
+abstract class RecordDatabase : RoomDatabase() {
+    abstract fun recordDao(): RecordDao
+
+    companion object {
+        private const val DATABASE_NAME = "record-database"
+        private var instance: RecordDatabase? = null
+
+        @Synchronized
+        fun getInstance(context: Context): RecordDatabase {
+            if (instance == null) {
+                synchronized(RecordDatabase::class) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        RecordDatabase::class.java,
+                        DATABASE_NAME
+                    ).build()
+                }
+            }
+            return instance!!
+        }
+    }
+}
