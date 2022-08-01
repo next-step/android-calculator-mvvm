2baa8b365e706f48419f6afda39f4a5f23c65976
Lee-ji-soo <nbsp3@naver.com>
2022-08-01 18:25:49 +0900

    create view model

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -2,10 +2,36 @@ package edu.nextstep.camp.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCounterBinding
+    private val viewModel: CounterViewModel by viewModels {
+        CounterViewModelFactory(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        binding = ActivityCounterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBinding();
+        initObserver();
     }
+
+    private fun initBinding() {
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+    }
+
+    private fun initObserver() {
+        viewModel.showErrorMessage.observe(this) {
+            it.consume()?.let { it ->
+                Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
