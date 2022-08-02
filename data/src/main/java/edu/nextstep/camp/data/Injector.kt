c212e91204c6f680e3d83f6a7e52c7a1a59a1319
1stpie <1stpie@naver.com>
2022-08-03 02:03:05 +0900

    MVVM 4단계 리팩터링 : 추가

--- data/src/main/java/edu/nextstep/camp/data/Injector.kt
+++ data/src/main/java/edu/nextstep/camp/data/Injector.kt
@@ -3,7 +3,7 @@ package edu.nextstep.camp.data
 import android.content.Context
 import edu.nextstep.camp.domain.calculator.CalculationRecordsRepository
 
-object DataModule {
+object Injector {
     fun provideCalculationRecordsRepository(context: Context): CalculationRecordsRepository {
         return AppDatabase.getInstance(context).calculationRecordDao().let {
             DefaultCalculationRecordsRepository(it)
