3c1313d95c801d3b028b2fbf8d7548caa4cd5f14
Sangmin Kim <48168117+Sangmeebee@users.noreply.github.com>
2022-08-02 22:35:43 +0900

    feat: gradle 세팅 & Counter를 MVVM패턴으로 구현 (#93)
    관련이슈: step1-Counter구현

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -1,11 +1,21 @@
 package edu.nextstep.camp.counter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCounterBinding
+    private val counterVM: CounterViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        binding = ActivityCounterBinding.inflate(layoutInflater).apply {
+            lifecycleOwner = this@CounterActivity
+            counterVM = this@CounterActivity.counterVM
+        }
+        setContentView(binding.root)
     }
 }
