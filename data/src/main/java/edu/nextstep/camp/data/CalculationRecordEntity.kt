e25b91c03c117334d6b6c7528a9292b5465972f9
1stpie <1stpie@naver.com>
2022-08-03 00:38:49 +0900

    MVVM 3단계 피드백 :  domain <-> data 간의 entity 변환 책임을 mapper 로 이동

--- data/src/main/java/edu/nextstep/camp/data/CalculationRecordEntity.kt
+++ data/src/main/java/edu/nextstep/camp/data/CalculationRecordEntity.kt
@@ -3,7 +3,6 @@ package edu.nextstep.camp.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import edu.nextstep.camp.domain.calculator.CalculationRecord
 import edu.nextstep.camp.domain.calculator.Expression
 
 @Entity(
@@ -13,14 +12,4 @@ internal data class CalculationRecordEntity(
     @ColumnInfo(name = "expression") val expression: Expression,
     @ColumnInfo(name = "result") val result: Int,
     @PrimaryKey(autoGenerate = true) val id: Int = 0
-) {
-    companion object {
-        @JvmStatic
-        fun of(calculationRecord: CalculationRecord) = CalculationRecordEntity(
-            expression = calculationRecord.expression,
-            result = calculationRecord.result,
-        )
-    }
-}
-
-internal fun CalculationRecordEntity.mapToDomain() = CalculationRecord(expression, result)
+)
--- data/src/main/java/edu/nextstep/camp/data/DefaultCalculationRecordsRepository.kt
+++ data/src/main/java/edu/nextstep/camp/data/DefaultCalculationRecordsRepository.kt
@@ -4,14 +4,15 @@ import edu.nextstep.camp.domain.calculator.CalculationRecord
 import edu.nextstep.camp.domain.calculator.CalculationRecordsRepository
 
 internal class DefaultCalculationRecordsRepository(
-    private val calculationRecordsDao: CalculationRecordsDao
+    private val calculationRecordsDao: CalculationRecordsDao,
+    private val calculationRecordMapper: CalculationRecordMapper = CalculationRecordMapper
 ) : CalculationRecordsRepository {
 
     override suspend fun saveCalculationRecord(vararg records: CalculationRecord) {
-        calculationRecordsDao.insertCalculationRecords(*records.map(CalculationRecordEntity::of).toTypedArray())
+        calculationRecordsDao.insertCalculationRecords(*records.map(calculationRecordMapper::mapToData).toTypedArray())
     }
 
     override suspend fun loadCalculationRecords(): List<CalculationRecord> {
-        return calculationRecordsDao.loadCalculationRecords().map(CalculationRecordEntity::mapToDomain).toList()
+        return calculationRecordsDao.loadCalculationRecords().map(calculationRecordMapper::mapToDomain).toList()
     }
 }
