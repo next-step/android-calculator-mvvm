4bee036796ee4f079aaa73fb50d79db1bcb32f08
sinoso <tpgus123123@naver.com>
2022-08-03 21:59:34 +0900

    refactor : code clean up

--- calculator/src/main/java/edu/nextstep/camp/calculator/DiffUtilItemCallbackForCalculationResult.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/DiffUtilItemCallbackForCalculationResult.kt
@@ -3,8 +3,7 @@ package edu.nextstep.camp.calculator
 import androidx.recyclerview.widget.DiffUtil
 import edu.nextstep.camp.calculator.domain.CalculationResult
 
-class DiffUtilItemCallbackForCalculationResult(
-) : DiffUtil.ItemCallback<CalculationResult>() {
+class DiffUtilItemCallbackForCalculationResult : DiffUtil.ItemCallback<CalculationResult>() {
     override fun areItemsTheSame(oldItem: CalculationResult, newItem: CalculationResult): Boolean {
         return oldItem == newItem
     }
