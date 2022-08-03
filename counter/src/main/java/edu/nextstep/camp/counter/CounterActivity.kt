64091972e9f8b80f0017ca862794af16a9e32d64
Sangmeebee <apfhdznzl@gmail.com>
2022-08-03 22:27:09 +0900

    feat: 0일경우 down 버튼 클릭시 toast 노출 관련이슈: step1-Counter구현

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -1,9 +1,12 @@
 package edu.nextstep.camp.counter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
+import kotlinx.coroutines.launch
 
 class CounterActivity : AppCompatActivity() {
 
@@ -17,5 +20,17 @@ class CounterActivity : AppCompatActivity() {
             counterVM = this@CounterActivity.counterVM
         }
         setContentView(binding.root)
+
+        observeShowToastEvent()
+    }
+
+    private fun observeShowToastEvent() = lifecycleScope.launch {
+        counterVM.showToastEvent.collect { message ->
+            showToast(message)
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
--- counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
@@ -2,7 +2,9 @@ package edu.nextstep.camp.counter
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
@@ -11,6 +13,9 @@ class CounterViewModel : ViewModel() {
     private val _count = MutableStateFlow(0)
     val count = _count.asStateFlow()
 
+    private val _showToastEvent = MutableSharedFlow<String>()
+    val showToastEvent = _showToastEvent.asSharedFlow()
+
     fun increment() = viewModelScope.launch {
         _count.emit(count.value + 1)
     }
@@ -18,6 +23,12 @@ class CounterViewModel : ViewModel() {
     fun decrement() = viewModelScope.launch {
         if (count.value > 0) {
             _count.emit(count.value - 1)
+        } else {
+            showToast("0 이하로 내릴 수 없습니다")
         }
+    }
+
+    fun showToast(message: String) = viewModelScope.launch {
+        _showToastEvent.emit(message)
     }
 }
