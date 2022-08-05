a89349789862c4f01ccf8dfeea2c94613e62f4f5
sinoso <tpgus123123@naver.com>
2022-08-05 17:05:19 +0900

    refactor : CalculatorActivity 계산기록에 맞춘 기능 단위 수정

--- calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
@@ -5,7 +5,6 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityCalculatorBinding
-import edu.nextstep.camp.calculator.domain.CalculationResult
 
 class CalculatorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCalculatorBinding
@@ -15,33 +14,27 @@ class CalculatorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculatorBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setBindingSettings()
-        setViewModelObservers()
-        setCalculationResultRecyclerView()
+        setUpBindingSettings()
+        setUpEventObserver()
+        setUpCalculationResult()
     }
 
-    private fun setBindingSettings() {
+    private fun setUpCalculationResult() {
+        val adapter = CalculationHistoryAdapter()
+        binding.recyclerView.adapter = adapter
+        viewModel.calculationResults.observe(this@CalculatorActivity) {
+            it?.let(adapter::submitList)
+        }
+    }
+
+    private fun setUpBindingSettings() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = this@CalculatorActivity
     }
 
-    private fun setViewModelObservers() {
+    private fun setUpEventObserver() {
         viewModel.event.observe(this@CalculatorActivity) {
             it.consume()?.let(::showToastWithCalculatorEvent)
-        }
-        viewModel.calculationResults.observe(this@CalculatorActivity) {
-            it?.let(::changeCalculateResults)
-        }
-    }
-
-    private fun changeCalculateResults(calculationResultList: List<CalculationResult>) {
-        val adapter = binding.recyclerView.adapter as CalculationHistoryAdapter
-        adapter.submitList(calculationResultList)
-    }
-
-    private fun setCalculationResultRecyclerView() {
-        binding.recyclerView.apply {
-            adapter = CalculationHistoryAdapter()
         }
     }
 
