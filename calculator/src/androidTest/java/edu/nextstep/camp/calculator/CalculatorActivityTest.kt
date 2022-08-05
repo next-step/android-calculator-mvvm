b8d0c9f2444252a0e918518e6ceac7093a1ea336
sinoso <tpgus123123@naver.com>
2022-08-05 16:07:35 +0900

    refactor : CalculatorActivityTest view가 보이지 않는 경우의 코드 수정

--- calculator/src/androidTest/java/edu/nextstep/camp/calculator/CalculatorActivityTest.kt
+++ calculator/src/androidTest/java/edu/nextstep/camp/calculator/CalculatorActivityTest.kt
@@ -4,8 +4,8 @@ import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.*
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.espresso.matcher.ViewMatchers.Visibility.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 
@@ -16,7 +16,7 @@ internal class CalculatorActivityTest {
     @Test
     fun 계산_기록이_보이지_않을_때_계산기록_버튼을_클릭하면_계산기록이_보인다() {
         //given 계산기록이 보이지 않을 때
-        Espresso.onView(withId(R.id.recyclerView)).check(matches(withEffectiveVisibility(GONE)))
+        Espresso.onView(withId(R.id.recyclerView)).check(matches(not(isDisplayed())))
 
         //when 계산기록 버튼이 눌리면
         Espresso.onView(withId(R.id.buttonMemory)).perform(click())
@@ -36,6 +36,6 @@ internal class CalculatorActivityTest {
         Espresso.onView(withId(R.id.buttonMemory)).perform(click())
 
         //then 계산기록이 보이지 않는다.
-        Espresso.onView(withId(R.id.recyclerView)).check(matches(withEffectiveVisibility(GONE)))
+        Espresso.onView(withId(R.id.recyclerView)).check(matches(not(isDisplayed())))
     }
 }
