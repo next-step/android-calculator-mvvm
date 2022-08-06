bdae1a60b4ecd193b5eec0d2594f329216773887
hyowan <woga1999@gmail.com>
2022-08-06 21:07:23 +0900

    fix: 안쓰는 파일 제거 및 주석 제거

--- data/src/main/java/edu/nextstep/camp/data/Injector.kt
+++
@@ -1,9 +0,0 @@
-package edu.nextstep.camp.data
-
-import edu.nextstep.camp.domain.LogRepository
-
-object Injector {
-    fun provideLogRepository(dao: LogDao): LogRepository {
-        return LogRepositoryImpl(dao)
-    }
-}
