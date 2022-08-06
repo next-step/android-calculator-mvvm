e16ac755e8102233eae23251c29d71f6e913960e
hyowan <woga1999@gmail.com>
2022-08-06 23:07:05 +0900

    fix: repositorycompoent 제거

--- data/src/main/java/edu/nextstep/camp/data/RepositoryComponent.kt
+++
@@ -1,8 +0,0 @@
-package edu.nextstep.camp.data
-
-import dagger.hilt.DefineComponent
-import dagger.hilt.components.SingletonComponent
-
-@DefineComponent(parent = SingletonComponent::class)
-interface RepositoryComponent {
-}
