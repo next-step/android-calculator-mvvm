0642311f030cc32ad5d70f9fac954eb4291f1759
ChoiYounho <maxosa78@gmail.com>
2022-08-10 15:50:02 +0900

    refactor : 컴파일 에러 해결

--- calculator/src/main/java/edu/nextstep/camp/calculator/HistoryViewHolder.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/HistoryViewHolder.kt
@@ -3,14 +3,14 @@ package edu.nextstep.camp.calculator
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import edu.nextstep.camp.calculator.data.CalculationHistoryEntity
 import edu.nextstep.camp.calculator.databinding.ItemResultBinding
+import edu.nextstep.camp.calculator.domain.History
 
-class HistoryViewHolder(
+class HistoryViewHolder private constructor(
     private val binding: ItemResultBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(history: CalculationHistoryEntity) {
+    fun bind(history: History) {
         binding.tvExpression.text = history.expression.toString()
         binding.tvResult.text = history.result
     }
