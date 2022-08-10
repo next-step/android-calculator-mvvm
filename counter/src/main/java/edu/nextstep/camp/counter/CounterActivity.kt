f7d64810df37385f190cfa9b2f6e598315ed87bf
박찬준 <chanjun.park@navercorp.com>
2022-08-10 22:22:11 +0900

    Counter UI 구현

--- counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterActivity.kt
@@ -2,10 +2,49 @@ package edu.nextstep.camp.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import edu.nextstep.camp.counter.databinding.ActivityCounterBinding
+import edu.nextstep.camp.domain.counter.Counter
 
 class CounterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCounterBinding
+    private lateinit var viewModel: CounterViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_counter)
+
+        initViewModel()
+
+        initBinding()
+
+        observeUiState()
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProvider(this, CounterViewModelFactory(Counter()))[CounterViewModel::class.java]
+    }
+
+    private fun initBinding() {
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+    }
+
+    private fun observeUiState() {
+        viewModel.counterUiState.observe(this) { counterUiState ->
+            counterUiState ?: return@observe
+
+            showErrorMessage(counterUiState.errorMessage)
+        }
+    }
+
+    private fun showErrorMessage(errorMessage: UiText?) {
+        errorMessage ?: return
+
+        Toast.makeText(this, errorMessage.asString(this), Toast.LENGTH_LONG).show()
+
+        viewModel.shownErrorMessage()
     }
 }
--- counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
+++ counter/src/main/java/edu/nextstep/camp/counter/CounterViewModel.kt
@@ -3,6 +3,7 @@ package edu.nextstep.camp.counter
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import edu.nextstep.camp.domain.counter.Counter
 import edu.nextstep.camp.domain.counter.NegativeCountNotSupported
 
@@ -36,5 +37,20 @@ class CounterViewModel(
         _counterUiState.value = _counterUiState.value?.copy(
             errorMessage = UiText.StringResource(R.string.negative_count_not_supported)
         )
+    }
+
+    fun shownErrorMessage() {
+        _counterUiState.value = _counterUiState.value?.copy(
+            errorMessage = null
+        )
+    }
+}
+
+class CounterViewModelFactory(private var counter: Counter): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(CounterViewModel::class.java)) {
+            return CounterViewModel(counter) as T
+        }
+        throw IllegalArgumentException("Not found ViewModel class.")
     }
 }
