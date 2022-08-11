c18bcc15748aeaa078727de917651e90037eb453
Sangmeebee <apfhdznzl@gmail.com>
2022-08-12 00:14:17 +0900

    feat: data모듈의 구현체 모두 internal 접근제어자 추가 & RepositoryInjector Coroutine Dispatcher 매개변수로 추가 & 테스트 코드 repository 초기화 로직 수정 관련이슈: step4-리팩터링

--- data/src/main/java/edu/nextstep/camp/calculator/data/di/RepositoryInjector.kt
+++ data/src/main/java/edu/nextstep/camp/calculator/data/di/RepositoryInjector.kt
@@ -3,9 +3,17 @@ package edu.nextstep.camp.calculator.data.di
 import edu.nextstep.camp.calculator.data.datasource.local.ExpressionHistoryLocalDataSource
 import edu.nextstep.camp.calculator.data.repository.ExpressionHistoryRepositoryImpl
 import edu.nextstep.camp.calculator.domain.repository.ExpressionHistoryRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 
 object RepositoryInjector {
 
-    fun provideExpressionHistoryRepository(expressionHistoryLocalDataSource: ExpressionHistoryLocalDataSource): ExpressionHistoryRepository =
-        ExpressionHistoryRepositoryImpl(expressionHistoryLocalDataSource)
+    fun provideExpressionHistoryRepository(
+        expressionHistoryLocalDataSource: ExpressionHistoryLocalDataSource,
+        ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
+    ): ExpressionHistoryRepository =
+        ExpressionHistoryRepositoryImpl(
+            expressionHistoryLocalDataSource = expressionHistoryLocalDataSource,
+            ioDispatcher = ioDispatcher
+        )
 }
