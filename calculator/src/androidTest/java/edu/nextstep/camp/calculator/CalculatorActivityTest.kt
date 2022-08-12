4ba6bc17c76b681a1d7ef2b66cc383b5150c9685
박찬준 <chanjun.park@navercorp.com>
2022-08-12 21:55:00 +0900

    파라미터 테스트 활용

--- calculator/src/androidTest/java/edu/nextstep/camp/calculator/CalculatorActivityTest.kt
+++ calculator/src/androidTest/java/edu/nextstep/camp/calculator/CalculatorActivityTest.kt
@@ -1,98 +1,33 @@
 package edu.nextstep.camp.calculator
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import org.junit.Rule
-import org.junit.Test
+import de.mannodermaus.junit5.ActivityScenarioExtension
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 @Suppress("NonAsciiCharacters")
-class CalculatorActivityTest {
-    @get:Rule
-    var activityScenarioRule = ActivityScenarioRule(CalculatorActivity::class.java)
+internal class CalculatorActivityTest {
+    @JvmField
+    @RegisterExtension
+    var activityScenarioRule = ActivityScenarioExtension.launch<CalculatorActivity>()
 
     // 사용자가 피연산자 0 ~ 9 버튼을 누르면 화면에 해당 숫자가 화면에 보여야 한다.
-    @Test
-    fun 버튼_0을_누르면_화면에_0이_보여야_한다() {
-        // when: 0 버튼을 누르면
-        onView(withId(R.id.button0)).perform(click())
-        // then: 화면에 0이 보여야 한다.
-        onView(withId(R.id.textView)).check(matches(withText("0")))
-    }
-
-    @Test
-    fun 버튼_1을_누르면_화면에_1이_보여야_한다() {
-        // when: 1 버튼을 누르면
-        onView(withId(R.id.button1)).perform(click())
-        // then: 화면에 1이 보여야 한다.
-        onView(withId(R.id.textView)).check(matches(withText("1")))
-    }
-
-    @Test
-    fun 버튼_2을_누르면_화면에_2이_보여야_한다() {
-        // when: 2 버튼을 누르면
-        onView(withId(R.id.button2)).perform(click())
-        // then: 화면에 2이 보여야 한다.
-        onView(withId(R.id.textView)).check(matches(withText("2")))
-    }
-
-    @Test
-    fun 버튼_3을_누르면_화면에_3이_보여야_한다() {
-        // when: 3 버튼을 누르면
-        onView(withId(R.id.button3)).perform(click())
-        // then: 화면에 3이 보여야 한다.
-        onView(withId(R.id.textView)).check(matches(withText("3")))
-    }
-
-    @Test
-    fun 버튼_4을_누르면_화면에_4이_보여야_한다() {
-        // when: 4 버튼을 누르면
-        onView(withId(R.id.button4)).perform(click())
-        // then: 화면에 4이 보여야 한다.
-        onView(withId(R.id.textView)).check(matches(withText("4")))
-    }
-
-    @Test
-    fun 버튼_5을_누르면_화면에_5이_보여야_한다() {
-        // when: 5 버튼을 누르면
-        onView(withId(R.id.button5)).perform(click())
-        // then: 화면에 5이 보여야 한다.
-        onView(withId(R.id.textView)).check(matches(withText("5")))
-    }
-
-    @Test
-    fun 버튼_6을_누르면_화면에_6이_보여야_한다() {
-        // when: 6 버튼을 누르면
-        onView(withId(R.id.button6)).perform(click())
-        // then: 화면에 6이 보여야 한다.
-        onView(withId(R.id.textView)).check(matches(withText("6")))
-    }
-
-    @Test
-    fun 버튼_7을_누르면_화면에_7이_보여야_한다() {
-        // when: 7 버튼을 누르면
-        onView(withId(R.id.button7)).perform(click())
-        // then: 화면에 7이 보여야 한다.
-        onView(withId(R.id.textView)).check(matches(withText("7")))
-    }
-
-    @Test
-    fun 버튼_8을_누르면_화면에_8이_보여야_한다() {
-        // when: 8 버튼을 누르면
-        onView(withId(R.id.button8)).perform(click())
-        // then: 화면에 8이 보여야 한다.
-        onView(withId(R.id.textView)).check(matches(withText("8")))
-    }
-
-    @Test
-    fun 버튼_9을_누르면_화면에_9이_보여야_한다() {
-        // when: 9 버튼을 누르면
-        onView(withId(R.id.button9)).perform(click())
-        // then: 화면에 9이 보여야 한다.
-        onView(withId(R.id.textView)).check(matches(withText("9")))
+    @ParameterizedTest
+    @ValueSource(ints = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    fun 숫자_버튼을_누르면_화면에_해당_숫자가_보여야_한다(number: Int) {
+        // when: 숫자 버튼을 누르면
+        onView(withText(number.toString())).perform(click())
+        // then: 화면에 해당 숫자가 보여야 한다.
+        onView(withId(R.id.textView)).check(matches(withText(number.toString())))
     }
 
     @Test
@@ -107,37 +42,11 @@ class CalculatorActivityTest {
         onView(withId(R.id.textView)).check(matches(withText("89")))
     }
 
-    @Test
-    fun 빈_화면에서_버튼_더하기를_누르면_화면에_아무런_변화가_없어야_한다() {
-        // when: 사용자가 연산자 + 버튼을 누르면
-        onView(withId(R.id.buttonPlus)).perform(click())
-
-        // then: 화면에 아무런 변화가 없어야 한다.
-        onView(withId(R.id.textView)).check(matches(withText("")))
-    }
-
-    @Test
-    fun 빈_화면에서_버튼_빼기를_누르면_화면에_아무런_변화가_없어야_한다() {
-        // when: 사용자가 연산자 - 버튼을 누르면
-        onView(withId(R.id.buttonMinus)).perform(click())
-
-        // then: 화면에 아무런 변화가 없어야 한다.
-        onView(withId(R.id.textView)).check(matches(withText("")))
-    }
-
-    @Test
-    fun 빈_화면에서_버튼_곱하기를_누르면_화면에_아무런_변화가_없어야_한다() {
-        // when: 사용자가 연산자 × 버튼을 누르면
-        onView(withId(R.id.buttonMultiply)).perform(click())
-
-        // then: 화면에 아무런 변화가 없어야 한다.
-        onView(withId(R.id.textView)).check(matches(withText("")))
-    }
-
-    @Test
-    fun 빈_화면에서_버튼_나누기를_누르면_화면에_아무런_변화가_없어야_한다() {
-        // when: 사용자가 연산자 ÷ 버튼을 누르면
-        onView(withId(R.id.buttonDivide)).perform(click())
+    @ParameterizedTest
+    @ValueSource(ints = [R.id.buttonPlus, R.id.buttonMinus, R.id.buttonMultiply, R.id.buttonDivide])
+    fun 빈_화면에서_사칙연산_버튼을_누르면_화면에_아무런_변화가_없어야_한다(operationStringResId: Int) {
+        // when: 사용자가 연산자 사칙연산 버튼을 누르면
+        onView(withId(operationStringResId)).perform(click())
 
         // then: 화면에 아무런 변화가 없어야 한다.
         onView(withId(R.id.textView)).check(matches(withText("")))
