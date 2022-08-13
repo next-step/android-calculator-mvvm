f7b3fce9be5484d5c0fe93ec1de6cccf621fbc97
박찬준 <chanjun.park@navercorp.com>
2022-08-13 21:26:41 +0900

    액티비티에 뷰모델 초기화 로직 추가, 계산 기록 UI 로직 추가

--- calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
+++ calculator/src/main/java/edu/nextstep/camp/calculator/CalculatorActivity.kt
@@ -5,10 +5,24 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityCalculatorBinding
+import edu.nextstep.camp.data.CalculationHistoryRepositoryImpl
+import edu.nextstep.camp.data.local.CalculatorDatabase
+import edu.nextstep.camp.domain.calculator.CalculationHistory
+import edu.nextstep.camp.domain.calculator.usecase.GetAllCalculationHistoryUseCase
+import edu.nextstep.camp.domain.calculator.usecase.InsertCalculationHistoryUseCase
 
 class CalculatorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCalculatorBinding
-    private val viewModel: CalculatorViewModel by viewModels()
+    private val viewModel: CalculatorViewModel by viewModels {
+        val database = CalculatorDatabase.getCalculatorDatabase(application)
+        val calculationHistoryRepository = CalculationHistoryRepositoryImpl(database.calculationHistoryEntityDao)
+        val insertCalculationHistoryUseCase = InsertCalculationHistoryUseCase(calculationHistoryRepository)
+        val getAllCalculationHistoryUseCase = GetAllCalculationHistoryUseCase(calculationHistoryRepository)
+
+        CalculatorViewModelFactory(insertCalculationHistoryUseCase, getAllCalculationHistoryUseCase)
+    }
+
+    private val calculationHistoryAdapter = CalculationHistoryAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +33,13 @@ class CalculatorActivity : AppCompatActivity() {
 
         initBinding()
 
+        initView()
+
         observeUiState()
+    }
+
+    private fun initView() {
+        binding.recyclerView.adapter = calculationHistoryAdapter
     }
 
     private fun initBinding() {
@@ -33,9 +53,19 @@ class CalculatorActivity : AppCompatActivity() {
 
             showErrorMessage(errorMessage)
         }
+
+        viewModel.calculationHistoryList.observe(this) { calculationHistoryList ->
+            calculationHistoryList ?: return@observe
+
+            setCalculationHistoryList(calculationHistoryList)
+        }
     }
 
     private fun showErrorMessage(errorMessage: UiText) {
         Toast.makeText(this, errorMessage.asString(this), Toast.LENGTH_LONG).show()
+    }
+
+    private fun setCalculationHistoryList(calculationHistoryList: List<CalculationHistory>) {
+        calculationHistoryAdapter.submitList(calculationHistoryList)
     }
 }
