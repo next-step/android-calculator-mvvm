d9bc80f8bc7063d3a5330805d9322d47a11357d3
JaeHwan <pon5976@naver.com>
2023-02-13 00:34:42 +0900

    feat: CounterActivity DataBinding 추가

--- counter-app/src/main/java/camp/nextstep/edu/counter/CounterActivity.kt
+++ counter-app/src/main/java/camp/nextstep/edu/counter/CounterActivity.kt
@@ -1,11 +1,27 @@
 package camp.nextstep.edu.counter
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import camp.nextstep.edu.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCounterBinding
+    private val counterViewModel: CounterViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        binding = ActivityCounterBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+            lifecycleOwner = this@CounterActivity
+            viewModel = counterViewModel
+        }
+        addObserve()
+    }
+
+    private fun addObserve() {
+        counterViewModel.isCounterLessThanZero.observe(this) {
+            Toast.makeText(this, R.string.cannot_be_less_than_zero, Toast.LENGTH_SHORT).show()
+        }
     }
 }
