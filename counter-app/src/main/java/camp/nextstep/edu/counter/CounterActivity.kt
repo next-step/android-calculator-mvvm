c5e8ec8ad79b5f150496bd5defb65b09e3310902
jpoh281 <hjp4869@naver.com>
2023-02-12 22:53:36 +0900

    feat: DataBinding, CounterActivity 설정

--- counter-app/src/main/java/camp/nextstep/edu/counter/CounterActivity.kt
+++ counter-app/src/main/java/camp/nextstep/edu/counter/CounterActivity.kt
@@ -1,11 +1,24 @@
 package camp.nextstep.edu.counter
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import camp.nextstep.edu.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCounterBinding
+    private val viewModel: CounterViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        val binding = ActivityCounterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        다음과 같이 사용가능
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_counter)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
     }
 }
