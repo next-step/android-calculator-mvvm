12ef6041a4e083587d4acdf5bbafec9bc353d442
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-02-13 13:51:23 +0900

    feat : 숫자 1씩 up & down 되는 기능 구현

--- counter-app/src/main/java/camp/nextstep/edu/counter/CounterViewModel.kt
+++ counter-app/src/main/java/camp/nextstep/edu/counter/CounterViewModel.kt
@@ -1,7 +1,25 @@
 package camp.nextstep.edu.counter
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CounterViewModel : ViewModel() {
+    private var _upDownUiState = MutableLiveData<Int>()
+    val upDownUiState: LiveData<Int> get() = _upDownUiState
 
+    init {
+        _upDownUiState.value = 0
+    }
+
+    fun increaseNumber() {
+        _upDownUiState.value = _upDownUiState.value?.plus(1)
+    }
+
+    fun decreaseNumber() {
+        if (upDownUiState.value == 0) {
+            //TODO: 0미만은 안 된다는 토스트 호출
+        }
+        _upDownUiState.value = _upDownUiState.value?.minus(1)
+    }
 }
