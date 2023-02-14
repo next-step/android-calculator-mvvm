74693b11f1c0e7c13461cda61589efd64906c52f
금 <pnurep@gmail.com>
2023-02-14 17:19:22 +0900

    * ViewModel 생성 (#121)
    * 로직 구현

--- counter-app/src/main/java/camp/nextstep/edu/counter/CounterActivity.kt
+++ counter-app/src/main/java/camp/nextstep/edu/counter/CounterActivity.kt
@@ -1,11 +1,54 @@
 package camp.nextstep.edu.counter
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import camp.nextstep.edu.counter.databinding.ActivityCounterBinding
+
 
 class CounterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCounterBinding
+
+    private val viewModel: CounterViewModel by viewModels()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+
+        initBinding()
+
+        observeViewModel()
+
+        setBindingVariables()
+    }
+
+    private fun initBinding() {
+        binding = DataBindingUtil.setContentView<ActivityCounterBinding>(
+            this, R.layout.activity_counter
+        ).apply {
+            lifecycleOwner = this@CounterActivity
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.warning.observe(this) {
+            makeToast()
+        }
+    }
+
+    private fun setBindingVariables() {
+        binding.setVariable(BR.viewModel, viewModel)
+        binding.executePendingBindings()
+    }
+
+    private fun makeToast() {
+        Toast.makeText(
+            applicationContext,
+            "0 이하로 내릴 수 없습니다",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
