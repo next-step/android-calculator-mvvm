a424ac8e616b88bac8094d8c5991bb4f39f30e6a
JaeHwan <pon5976@naver.com>
2023-02-14 19:57:13 +0900

    feat: Calculator ViewModel 생성 및 MVP 코드 제거

---
+++ calculator-app/src/main/java/camp/nextstep/edu/calculator/CalculatorViewModel.kt
@@ -0,0 +1,12 @@
+package camp.nextstep.edu.calculator
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import camp.nextstep.edu.calculator.domain.Expression
+import camp.nextstep.edu.calculator.domain.Operator
+
+class CalculatorViewModel : ViewModel() {
+
+
+}
