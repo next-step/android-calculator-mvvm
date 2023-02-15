23f18c362330088f323dd422c9c65f31d1dd17bd
jpoh281 <hjp4869@naver.com>
2023-02-16 00:14:24 +0900

    refactor: app -> calculator app, domain -> calculator domain

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,4 @@
 rootProject.name = "AndroidCalculator"
-include(":calculator-app")
 include(":counter-app")
+include(":calculator-app")
 include(":calculator-domain")
-include(":app")
-include(":domain")
