ffbbd3cbda495634d0d7e34329d9ca740ede4367
JaeHwan <pon5976@naver.com>
2023-02-16 22:00:25 +0900

    feat: CalculatorResultRepository 구현 추가

--- calculator-data/src/main/java/camp/nextstep/edu/calculator/local/impl/CalculatorResultRepositoryImpl.kt
+++ calculator-data/src/main/java/camp/nextstep/edu/calculator/local/impl/CalculatorResultRepositoryImpl.kt
@@ -2,14 +2,27 @@ package camp.nextstep.edu.calculator.local.impl
 
 import camp.nextstep.edu.calculator.domain.model.CalculatorResultData
 import camp.nextstep.edu.calculator.domain.repository.CalculatorResultRepository
-import camp.nextstep.edu.calculator.local.db.CalculatorDatabase
+import camp.nextstep.edu.calculator.local.dao.CalculatorResultDao
+import camp.nextstep.edu.calculator.local.entity.CalculatorResultEntity
 
-class CalculatorResultRepositoryImpl(private val db : CalculatorDatabase) : CalculatorResultRepository {
+class CalculatorResultRepositoryImpl(
+    private val dao: CalculatorResultDao,
+) : CalculatorResultRepository {
     override fun saveResult(calculatorResultData: CalculatorResultData) {
-        TODO("Not yet implemented")
+        dao.save(
+            CalculatorResultEntity(
+                expression = calculatorResultData.expression,
+                answer = calculatorResultData.answer
+            )
+        )
     }
 
     override fun getAllResult(): List<CalculatorResultData> {
-        TODO("Not yet implemented")
+        return dao.getResultList().map {
+            CalculatorResultData(
+                expression = it.expression,
+                answer = it.answer
+            )
+        }
     }
 }
