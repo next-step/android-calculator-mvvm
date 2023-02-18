97ab3f7d954422d66c5b419d095dea157df728e9
JaeHwan <pon5976@naver.com>
2023-02-18 16:25:50 +0900

    refactor: CalculatorResultViewHolder inner 클래스 제거 후 분리

--- calculator-app/src/main/java/camp/nextstep/edu/calculator/CalculatorResultAdapter.kt
+++ calculator-app/src/main/java/camp/nextstep/edu/calculator/CalculatorResultAdapter.kt
@@ -4,20 +4,10 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
-import camp.nextstep.edu.calculator.CalculatorResultAdapter.CalculatorResultViewHolder
 import camp.nextstep.edu.calculator.databinding.ItemResultBinding
 import camp.nextstep.edu.calculator.domain.model.CalculatorResultData
 
 class CalculatorResultAdapter : ListAdapter<CalculatorResultData, CalculatorResultViewHolder>(diff) {
-    inner class CalculatorResultViewHolder(
-        private val binding: ItemResultBinding
-        ) : RecyclerView.ViewHolder(binding.root) {
-            fun bind(data: CalculatorResultData) {
-                binding.data = data
-            }
-    }
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalculatorResultViewHolder {
         val binding = ItemResultBinding.inflate(LayoutInflater.from(parent.context), parent, false)
