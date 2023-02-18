cf76cbe6d2dee1c201fa8f0938bfe900b3205b73
Soyeon Park <43838030+paksuua@users.noreply.github.com>
2023-02-18 23:58:07 +0900

    2단계: MVVM 변환 (#129)
    * update: [리뷰반영] 테스트를 위해 추가했던 프로덕션 코드 제거
    
    * add: 계산기 UI 테스트 코드 추가
    
    * refactor: 비즈니스 로직 이동 Presenter -> ViewModel
    
    * chore: 계산기 UI 테스트 코드 수정
    
    * fix: 계산 결과 함수 오류 수정
    
    * add: 계산기 버튼에 Click 이벤트 연결
    
    * update: ViewModel 함수 early return 적용 및 역할이 중복되는 함수 제거
    
    * add: 계산기 ViewModel 테스트 코드 추가
    
    * remove: 사용자지 않는 Contract, Presenter 제거

---
+++ calculator-app/src/main/java/camp/nextstep/edu/calculator/CalculatorViewModel.kt
@@ -0,0 +1,52 @@
+package camp.nextstep.edu.calculator
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import camp.nextstep.edu.calculator.domain.Calculator
+import camp.nextstep.edu.calculator.domain.Expression
+import camp.nextstep.edu.calculator.domain.Operator
+
+class CalculatorViewModel : ViewModel() {
+
+    private val calculator = Calculator()
+
+    private val _expression = MutableLiveData<Expression>(Expression.EMPTY)
+    val expression: LiveData<Expression> = _expression
+
+    private val _result = MutableLiveData<String>()
+    val result: LiveData<String> = _result
+
+    private val _showIncompleteExpressionError: SingleLiveEvent<Boolean> = SingleLiveEvent()
+    val showIncompleteExpressionError: LiveData<Boolean> = _showIncompleteExpressionError
+
+    fun addToExpression(operand: Int) {
+        val expression = _expression.value ?: Expression.EMPTY
+        showExpression(expression.plus(operand))
+    }
+
+    fun addToExpression(operator: Operator) {
+        val expression = _expression.value ?: return
+        showExpression(expression.plus(operator))
+    }
+
+    fun removeLast() {
+        val expression = _expression.value ?: return
+        showExpression(expression.removeLast())
+    }
+
+    fun calculate() {
+        val result = calculator.calculate(_expression.value.toString())
+        if (result == null) {
+            _showIncompleteExpressionError.value = true
+        } else {
+            val expression = Expression(listOf(result))
+            showExpression(expression)
+            _showIncompleteExpressionError.value = false
+        }
+    }
+
+    private fun showExpression(expression: Expression) {
+        _expression.value = expression
+    }
+}
--- counter-app/src/test/java/camp/nextstep/edu/counter/CounterTest.kt
+++ counter-app/src/test/java/camp/nextstep/edu/counter/CounterTest.kt
@@ -12,9 +12,29 @@ class CounterTest {
     val instantExecutorRule = InstantTaskExecutorRule()
 
     @Test
+    fun `count의 초기 값은 0이다`() {
+        // given :
+        // when :
+        // than :
+        val actualExpression = viewModel.count.getOrAwaitValue()
+        assertThat(actualExpression).isEqualTo(0)
+    }
+
+    @Test
+    fun `count 0에서, UP 버튼을 누르면, count가 1이 된다`() {
+        // given :
+        // when :
+        viewModel.upCount()
+        // than :
+        val actualExpression = viewModel.count.getOrAwaitValue()
+        assertThat(actualExpression).isEqualTo(1)
+    }
+
+    @Test
     fun `count 2에서, UP 버튼을 누르면, count가 3이 된다`() {
         // given :
-        viewModel.setCount(2)
+        viewModel.upCount()
+        viewModel.upCount()
         // when :
         viewModel.upCount()
         // than :
@@ -25,7 +45,8 @@ class CounterTest {
     @Test
     fun `count 2에서 DOWN 버튼을 누르면 count가 1이 된다`() {
         // given :
-        viewModel.setCount(2)
+        viewModel.upCount()
+        viewModel.upCount()
         // when :
         viewModel.downCount()
         // than :
@@ -36,7 +57,6 @@ class CounterTest {
     @Test
     fun `count 0에서 DOWN 버튼을 누르면 count가 0이 된다`() {
         // given :
-        viewModel.setCount(0)
         // when :
         viewModel.downCount()
         // than :
