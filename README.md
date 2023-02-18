3cab427634d2846dd27770f2860fc9a3a214aae6
James <bookwriterseo@gmail.com>
2023-02-19 03:04:16 +0900

    feat: Databinding을 위한 View 연결

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### 1단계
 - [x] 패키지 구조 설정
+- [x] Databinding을 위한 View 연결
+- [ ] Counter 로직을 적용한 ViewModel 구현
+- [ ] Databinding을 위한 ViewModel 연결
--- counter-app/src/main/java/camp/nextstep/edu/counter/view/CounterActivity.kt
+++ counter-app/src/main/java/camp/nextstep/edu/counter/view/CounterActivity.kt
@@ -1,17 +1,21 @@
 package camp.nextstep.edu.counter.view
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import camp.nextstep.edu.counter.R
+import camp.nextstep.edu.counter.databinding.ActivityCounterBinding
 import camp.nextstep.edu.counter.viewmodel.CounterViewModel
 
 class CounterActivity : AppCompatActivity() {
-    private lateinit var viewModel: CounterViewModel
+    private val viewModel: CounterViewModel by viewModels()
+    private lateinit var binding: ActivityCounterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
-
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_counter)
+        binding.lifecycleOwner = this
+        binding.counterView = viewModel
     }
 }
