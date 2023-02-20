df26328bdfc378cbd797cf9be94b657f2896516a
jyuiki <jyuiki@gmail.com>
2023-02-20 15:15:18 +0900

    step1 : Counter 구현

--- counter-app/src/main/java/camp/nextstep/edu/counter/CounterActivity.kt
+++ counter-app/src/main/java/camp/nextstep/edu/counter/CounterActivity.kt
@@ -1,11 +1,26 @@
 package camp.nextstep.edu.counter
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import camp.nextstep.edu.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCounterBinding
+    private val viewModel: CounterViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+        binding = ActivityCounterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        viewModel.onEvent.observe(this) {
+            Toast.makeText(this, R.string.notice_cannot_below_0, Toast.LENGTH_SHORT).show()
+        }
     }
 }
