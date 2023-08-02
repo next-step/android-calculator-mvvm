c028723c8489d6cc50f3680db35707a7b818733b
Yoon SungHyun <mioyo2@gmail.com>
2023-08-03 00:55:55 +0900

    step1: Counter 구현 (#145)
    * feat: 단계 기능 구현
    
    * refactor: ktlint 적용
    
    ---------
    
    Co-authored-by: 1002533 <bf.winfive@sk.com>

---
+++ counter-app/src/main/java/camp/nextstep/edu/counter/domain/Counter.kt
@@ -0,0 +1,27 @@
+package camp.nextstep.edu.counter.domain
+
+@JvmInline
+value class Counter(val number: Int = MINIMUM_NUMBER) {
+
+    init {
+        require(number >= MINIMUM_NUMBER) {
+            "숫자는 0이상 이어야함"
+        }
+    }
+
+    override fun toString(): String {
+        return number.toString()
+    }
+
+    operator fun plus(i: Int): Counter {
+        return Counter(number + i)
+    }
+
+    operator fun minus(i: Int): Counter {
+        return Counter(number - i)
+    }
+
+    companion object {
+        const val MINIMUM_NUMBER = 0
+    }
+}
---
+++ counter-app/src/test/java/camp/nextstep/edu/counter/domain/CounterTest.kt
@@ -0,0 +1,27 @@
+package camp.nextstep.edu.counter.domain
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Assert.assertThrows
+import org.junit.Test
+
+class CounterTest {
+
+    @Test
+    fun `0보다 작은 음수는 올 수 없다`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            Counter(-1)
+        }
+    }
+
+    @Test
+    fun `0에서 1 증가`() {
+        val actual = Counter(0)
+        assertThat(actual + 1).isEqualTo(Counter(1))
+    }
+
+    @Test
+    fun `1에서 0 감소`() {
+        val actual = Counter(1)
+        assertThat(actual - 1).isEqualTo(Counter(0))
+    }
+}
