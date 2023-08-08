4a78ee5711c66d89fee37b2f7046b8ae3501fbe5
김영욱 <0.uk.kim@navercorp.com>
2023-08-08 23:41:27 +0900

    CounterActivity, CounterViewModel 연결

--- counter-app/src/main/java/camp/nextstep/edu/counter/CounterActivity.kt
+++ counter-app/src/main/java/camp/nextstep/edu/counter/CounterActivity.kt
@@ -1,11 +1,36 @@
 package camp.nextstep.edu.counter
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import camp.nextstep.edu.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
-    }
+	private lateinit var binding: ActivityCounterBinding
+	private val counterViewModel: CounterViewModel by viewModels()
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+		binding = ActivityCounterBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+
+		binding.lifecycleOwner = this
+		binding.viewModel = counterViewModel
+
+		binding.buttonUp.setOnClickListener {
+			counterViewModel.countUp()
+		}
+		binding.buttonDown.setOnClickListener {
+			val isSuccess = counterViewModel.countDown()
+			showCountDownFailMessageIfNeed(isSuccess)
+		}
+	}
+
+	private fun showCountDownFailMessageIfNeed(isSuccess: Boolean) {
+		if (isSuccess.not()) {
+			Toast.makeText(this, "0 이하로 내릴 수 없습니다", Toast.LENGTH_LONG).show()
+		}
+	}
 }
