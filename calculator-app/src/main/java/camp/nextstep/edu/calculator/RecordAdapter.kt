403ad077e9083b33387f98c076b12b1fdab2dd1d
김영욱 <0.uk.kim@navercorp.com>
2023-08-11 00:12:27 +0900

    RecordAdapter 구현 및 연결

---
+++ calculator-app/src/main/java/camp/nextstep/edu/calculator/RecordAdapter.kt
@@ -0,0 +1,39 @@
+package camp.nextstep.edu.calculator
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import camp.nextstep.edu.calculator.databinding.ItemResultBinding
+import camp.nextstep.edu.calculator.domain.RecordResource
+
+class RecordAdapter : ListAdapter<RecordResource, RecordAdapter.RecordViewHolder>(recordDiffUtil) {
+
+	class RecordViewHolder(private val binding: ItemResultBinding): RecyclerView.ViewHolder(binding.root) {
+
+		fun bind(recorderResource: RecordResource) {
+			binding.recordResource = recorderResource
+		}
+	}
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordViewHolder {
+		return RecordViewHolder(ItemResultBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+	}
+
+	override fun onBindViewHolder(holder: RecordViewHolder, position: Int) {
+		holder.bind(getItem(position))
+	}
+
+	companion object {
+		val recordDiffUtil = object : DiffUtil.ItemCallback<RecordResource>() {
+			override fun areItemsTheSame(oldItem: RecordResource, newItem: RecordResource): Boolean {
+				return oldItem.id == newItem.id
+			}
+
+			override fun areContentsTheSame(oldItem: RecordResource, newItem: RecordResource): Boolean {
+				return oldItem == newItem
+			}
+		}
+	}
+}
