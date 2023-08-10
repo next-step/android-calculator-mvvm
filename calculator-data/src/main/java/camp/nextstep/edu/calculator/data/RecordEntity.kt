b48a6f7a1b290d95dadb8ece7a1557065270e0ec
김영욱 <0.uk.kim@navercorp.com>
2023-08-10 23:02:21 +0900

    계산기록에 대한 데이터 모델 생성

---
+++ calculator-data/src/main/java/camp/nextstep/edu/calculator/data/RecordEntity.kt
@@ -0,0 +1,20 @@
+package camp.nextstep.edu.calculator.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import camp.nextstep.edu.calculator.domain.Expression
+import camp.nextstep.edu.calculator.domain.RecordResource
+
+@Entity
+data class RecordEntity(
+	@PrimaryKey(autoGenerate = true) val id: Int = 0,
+	@ColumnInfo(name = "expression") val expression: String,
+	@ColumnInfo(name = "result") val result: Int,
+)
+
+fun RecordEntity.asRecordResource() = RecordResource(
+	id = id,
+	expression = Expression.of(expression),
+	result = result
+)
