2d6ce74a09475b026e2de45d0aa4e932494eeadf
minhyukseul <mioyo2@gmail.com>
2023-08-20 21:07:00 +0900

    refactor: boilerplate Code 제거 및 사용하지 않는 import 제거

--- calculator-data/src/main/java/camp/nextstep/edu/calculator/data/HistoryDatabase.kt
+++ calculator-data/src/main/java/camp/nextstep/edu/calculator/data/HistoryDatabase.kt
@@ -1,8 +1,6 @@
 package camp.nextstep.edu.calculator.data
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [HistoryEntity::class], version = 1, exportSchema = false)
