353a1e97ea0c021a5a2ad0c951b2f8b8974f0245
1002533 <bf.winfive@sk.com>
2023-08-21 10:33:04 +0900

    bugfix: coroutine 테스트를 위한 코드 수정

--- calculator-app/build.gradle.kts
+++ calculator-app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
     testImplementation("com.google.truth:truth:1.1.4")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
     testImplementation("io.mockk:mockk:1.10.6")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- calculator-app/src/test/java/camp/nextstep/edu/calculator/viewmodel/CalculatorViewModelTest.kt
+++ calculator-app/src/test/java/camp/nextstep/edu/calculator/viewmodel/CalculatorViewModelTest.kt
@@ -1,11 +1,20 @@
 package camp.nextstep.edu.calculator.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.viewModelScope
+import camp.nextstep.edu.calculator.domain.Calculator
 import camp.nextstep.edu.calculator.domain.Expression
 import camp.nextstep.edu.calculator.domain.Operator
 import camp.nextstep.edu.calculator.domain.repo.HistoryRepository
 import com.google.common.truth.Truth.assertThat
 import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -16,29 +25,36 @@ class CalculatorViewModelTest {
 
     private lateinit var viewModel: CalculatorViewModel
     private lateinit var repository: HistoryRepository
+    private val calculator = Calculator()
 
     @Before
     fun setUp() {
+        Dispatchers.setMain(Dispatchers.Unconfined)
         repository = mockk(relaxed = true)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
     }
 
     @Test
     fun `입력된 피연산자가 없을 때 1클릭하면 1이 보여야 함`() {
-        viewModel = CalculatorViewModel(Expression(listOf(1)), repository)
+        viewModel = CalculatorViewModel(Expression(listOf(1)), calculator, repository)
         val actual = viewModel.expression.value.toString()
         assertThat(actual).isEqualTo("1")
     }
 
     @Test
     fun `5 + 1 클릭하면 5 + 1이 보여야함`() {
-        viewModel = CalculatorViewModel(Expression(listOf(5, Operator.Plus, 1)), repository)
+        viewModel = CalculatorViewModel(Expression(listOf(5, Operator.Plus, 1)), calculator, repository)
         val actual = viewModel.expression.value.toString()
         assertThat(actual).isEqualTo("5 + 1")
     }
 
     @Test
     fun `입력된 피연산자가 없을 때, +, -, ×, ÷ 버튼을 누르면 화면에 아무런 변화가 없어야 한다`() {
-        viewModel = CalculatorViewModel(Expression(listOf()), repository)
+        viewModel = CalculatorViewModel(Expression(listOf()), calculator, repository)
         viewModel.addToExpression(Operator.Plus)
         viewModel.addToExpression(Operator.Minus)
         viewModel.addToExpression(Operator.Multiply)
@@ -49,14 +65,14 @@ class CalculatorViewModelTest {
 
     @Test
     fun `입력된 피연산자가 있을 때, +를 누르면 해당 화면이 보여야함  1 +은 1+1이 보여야함`() {
-        viewModel = CalculatorViewModel(Expression(listOf(1, Operator.Plus)), repository)
+        viewModel = CalculatorViewModel(Expression(listOf(1, Operator.Plus)), calculator, repository)
         val actual = viewModel.expression.value.toString()
         assertThat(actual).isEqualTo("1 +")
     }
 
     @Test
     fun `식이 없을 때 사용자가 지우기 버튼을 누르면 화면에 아무런 변화가 없어야 한다`() {
-        viewModel = CalculatorViewModel(repository = repository)
+        viewModel = CalculatorViewModel(Expression.EMPTY, calculator, repository)
         viewModel.removeLast()
         val actual = viewModel.expression.value.toString()
         assertThat(actual).isEqualTo("")
@@ -64,7 +80,7 @@ class CalculatorViewModelTest {
 
     @Test
     fun `입력된 수식이 있을 때, 사용자가 지우기 버튼을 누르면 수식에 마지막으로 입력된 연산자 또는 피연산자가 지워져야 한다`() {
-        viewModel = CalculatorViewModel(Expression(listOf(32, Operator.Plus, 1)), repository)
+        viewModel = CalculatorViewModel(Expression(listOf(32, Operator.Plus, 1)), calculator, repository)
         var actual = viewModel.expression.value.toString()
         assertThat(actual).isEqualTo("32 + 1")
         viewModel.removeLast()
@@ -83,7 +99,7 @@ class CalculatorViewModelTest {
 
     @Test
     fun `입력된 수신이 완전할 때, 사용자가 = 버튼을 누르면 입력된 수식의 결과가 화면에 보여야 한다 3 + 2 = 5`() {
-        viewModel = CalculatorViewModel(Expression(listOf(3, Operator.Plus, 2)), repository)
+        viewModel = CalculatorViewModel(Expression(listOf(3, Operator.Plus, 2)), calculator, repository)
         viewModel.calculate()
         val actual = viewModel.expression.value.toString()
         assertThat(actual).isEqualTo("5")
@@ -91,7 +107,7 @@ class CalculatorViewModelTest {
 
     @Test
     fun `입력된 수식이 완전하지 않을 때, 사용자가 = 버튼을 눌렀을 때 완성되지 않은 수식입니다 에러발생`() {
-        viewModel = CalculatorViewModel(Expression(listOf(3, Operator.Plus)), repository)
+        viewModel = CalculatorViewModel(Expression(listOf(3, Operator.Plus)), calculator, repository)
 
         viewModel.calculate()
         val actual = viewModel.uiState.value
