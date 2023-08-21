51cc8d2b11b5550d43a80f91cf1aec67cffac9ff
OYJ <oyj7677@gmail.lcom>
2023-08-21 23:32:09 +0900

    리사이클러를 사용한 계산 결과 기록 보기 기능 추가

--- calculator-app/src/main/java/camp/nextstep/edu/calculator/CalculatorActivity.kt
+++ calculator-app/src/main/java/camp/nextstep/edu/calculator/CalculatorActivity.kt
@@ -1,24 +1,36 @@
 package camp.nextstep.edu.calculator
 
+import android.annotation.SuppressLint
+import android.opengl.Visibility
 import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.recyclerview.widget.LinearLayoutManager
 import camp.nextstep.edu.calculator.databinding.ActivityCalculatorBinding
-import camp.nextstep.edu.calculator.domain.Expression
-import camp.nextstep.edu.calculator.domain.Operator
+import com.example.calculator.data.CalculatorDatabase
 
 class CalculatorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCalculatorBinding
+    private lateinit var adapter: Adpter
     private val viewModel: CalculatorViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        baseContext
         initBinding()
         initViewModelObserve()
+        initRecyclerView()
         setContentView(binding.root)
+        viewModel.setCalculatorDao(CalculatorDatabase.getInstance(this)!!)
+    }
+
+    private fun initRecyclerView() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = Adpter()
+        binding.recyclerView.adapter = adapter
     }
 
     private fun initBinding() {
@@ -27,10 +39,26 @@ class CalculatorActivity : AppCompatActivity() {
         binding.viewModel = viewModel
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     private fun initViewModelObserve() {
         viewModel.toastEvent.observe(this) { strRes ->
             val msg = getString(strRes)
             Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
         }
-    }
+
+        viewModel.memoryList.observe(this) {
+            adapter.setMemoryList(it)
+            adapter.notifyDataSetChanged()
+        }
+
+        viewModel.showHistory.observe(this) { isSwhoHistory ->
+            if(isSwhoHistory) {
+                binding.recyclerView.visibility = View.VISIBLE
+                binding.textView.visibility = View.INVISIBLE
+            } else {
+                binding.recyclerView.visibility = View.GONE
+                binding.textView.visibility = View.VISIBLE
+            }
+        }
+     }
 }
--- calculator-app/src/main/java/camp/nextstep/edu/calculator/CalculatorViewModel.kt
+++ calculator-app/src/main/java/camp/nextstep/edu/calculator/CalculatorViewModel.kt
@@ -5,11 +5,20 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import camp.nextstep.edu.calculator.domain.Calculator
 import camp.nextstep.edu.calculator.domain.Expression
+import camp.nextstep.edu.calculator.domain.Memory
 import camp.nextstep.edu.calculator.domain.Operator
-import kotlin.coroutines.coroutineContext
+import com.example.calculator.data.CalculatorDao
+import com.example.calculator.data.CalculatorDatabase
+import com.example.calculator.data.MemoryEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
 
 class CalculatorViewModel(initFormula: List<Any> = emptyList()) : ViewModel() {
     private val calculator = Calculator()
+    private lateinit var calculatorDao: CalculatorDao
+    private var isShowHistory = false
 
     private var _formula = MutableLiveData(Expression(initFormula))
     val formula: LiveData<Expression>
@@ -18,6 +27,14 @@ class CalculatorViewModel(initFormula: List<Any> = emptyList()) : ViewModel() {
     private var _toastEvent = SingleLiveEvent<Int>()
     val toastEvent: LiveData<Int>
         get() = _toastEvent
+
+    private var _memoryList = MutableLiveData<List<Memory>>()
+    val memoryList: LiveData<List<Memory>>
+        get() = _memoryList
+
+    private var _showHistoryEvent = SingleLiveEvent<Boolean>()
+    val showHistory: LiveData<Boolean>
+        get() = _showHistoryEvent
 
     fun addToExpression(operand: Int) {
         _formula.value = _formula.value?.plus(operand)
@@ -36,7 +53,44 @@ class CalculatorViewModel(initFormula: List<Any> = emptyList()) : ViewModel() {
         if (result == null) {
             _toastEvent.value = R.string.incomplete_expression
         } else {
+            val formula = _formula.value.toString()
             _formula.value = Expression(listOf(result))
+
+            CoroutineScope(Dispatchers.IO).launch {
+                calculatorDao.insertMemory(
+                    MemoryEntity(
+                        expression = formula,
+                        result = "= $result"
+                    )
+                )
+            }
         }
+    }
+
+    fun getHistory() {
+        if(isShowHistory) {
+            isShowHistory = false
+            _showHistoryEvent.value = isShowHistory
+        } else {
+            isShowHistory = true
+            CoroutineScope(Dispatchers.IO).launch {
+                val memoryList = arrayListOf<Memory>()
+                for (memory in calculatorDao.getMemories()) {
+                    memoryList.add(
+                        Memory(
+                            expression = memory.expression,
+                            result = memory.result
+                        )
+                    )
+                }
+                _showHistoryEvent.postValue(isShowHistory)
+                _memoryList.postValue(memoryList)
+            }
+        }
+
+    }
+
+    fun setCalculatorDao(calculatorDatabase: CalculatorDatabase) {
+        calculatorDao = calculatorDatabase.CalculatorDao()
     }
 }
