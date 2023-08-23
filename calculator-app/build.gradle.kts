5bde16b0df2f17ad625d6dadcfa137dd8dd2e00b
OYJ <oyj7677@gmail.lcom>
2023-08-24 01:11:24 +0900

    Step3 1차 추가 개선 사항 에러 수정 - ViewModel 단위 테스트 시 사용된 mockk 의존성 주입 커밋 누락

--- calculator-app/build.gradle.kts
+++ calculator-app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("com.google.truth:truth:1.1.3")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("io.mockk:mockk:1.13.5")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
