4b62e8c25c102891f14a91f8a5f683198866b0c0
KwonDae <green201402317@gmail.com>
2023-08-24 22:34:00 +0900

    데이터 바인딩 연동, Counter 구현

--- counter-app/src/main/java/camp/nextstep/edu/counter/CounterActivity.kt
+++ counter-app/src/main/java/camp/nextstep/edu/counter/CounterActivity.kt
@@ -1,11 +1,38 @@
 package camp.nextstep.edu.counter
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import camp.nextstep.edu.counter.databinding.ActivityCounterBinding
 
 class CounterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCounterBinding
+    private val viewModel: CounterViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counter)
+
+        initBinding()
+        initObserver()
+    }
+
+    private fun initBinding() {
+        binding = ActivityCounterBinding.inflate(layoutInflater)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        setContentView(binding.root)
+    }
+
+    private fun initObserver() {
+        viewModel.uiEffect.observe(this) { uiEffect ->
+            when (uiEffect) {
+                is UiEffect.Error -> Toast.makeText(this, uiEffect.message, Toast.LENGTH_SHORT)
+                    .show()
+
+                else -> {}
+            }
+        }
     }
 }
--- counter-app/src/main/java/camp/nextstep/edu/counter/CounterViewModel.kt
+++ counter-app/src/main/java/camp/nextstep/edu/counter/CounterViewModel.kt
@@ -1,10 +1,35 @@
 /**
  * @author Daewon on 24,August,2023
- * 
+ *
  */
- 
+
 package camp.nextstep.edu.counter
 
+import androidx.lifecycle.ViewModel
 
-class CounterViewModel {
+
+class CounterViewModel : ViewModel() {
+    private val _count = SingleLiveEvent<Int>().apply { value = 0 }
+    val count: SingleLiveEvent<Int>
+        get() = _count
+
+    private val _uiEffect = SingleLiveEvent<UiEffect>()
+    val uiEffect: SingleLiveEvent<UiEffect>
+        get() = _uiEffect
+
+    fun increase() {
+        _count.value = _count.value?.plus(1)
+    }
+
+    fun decrease() {
+        if (count.value!! <= 0) {
+            _uiEffect.value = UiEffect.Error("0 이하로 내릴 수 없습니다.")
+        } else {
+            _count.value = _count.value?.minus(1)
+        }
+    }
+}
+
+sealed interface UiEffect {
+    data class Error(val message: String) : UiEffect
 }
