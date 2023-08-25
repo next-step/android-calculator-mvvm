79a4a6dd595e2d5543bfe6a52c3e5be6eee4fcd1
KwonDae <green201402317@gmail.com>
2023-08-25 17:44:34 +0900

    ViewModel 계산 로직 구현

--- calculator-app/src/main/java/camp/nextstep/edu/calculator/CalculatorViewModel.kt
+++ calculator-app/src/main/java/camp/nextstep/edu/calculator/CalculatorViewModel.kt
@@ -3,27 +3,63 @@ package camp.nextstep.edu.calculator
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import camp.nextstep.edu.calculator.domain.Calculator
 import camp.nextstep.edu.calculator.domain.Expression
 import camp.nextstep.edu.calculator.domain.Operator
 
 class CalculatorViewModel : ViewModel() {
-    private val _expression = MutableLiveData<Expression>().apply { value = Expression.EMPTY }
-    val expression: LiveData<Expression>
-        get() = _expression
+    private val calculator = Calculator()
+    private var expression = Expression.EMPTY
+    private val _result = MutableLiveData<String>().apply { value = "" }
+    val result: LiveData<String>
+        get() = _result
+
+    private val _uiEffect = SingleLiveEvent<UiEffect>()
+    val uiEffect: LiveData<UiEffect>
+        get() = _uiEffect
 
     fun addToExpression(operand: Int) {
-
+        runCatching {
+            expression += operand
+        }.onSuccess {
+            _result.value = expression.toString()
+        }.onFailure {
+            _uiEffect.value = UiEffect.ShowErrorMessage(it.message)
+        }
     }
 
     fun addToExpression(operator: Operator) {
-
+        runCatching {
+            expression += operator
+        }.onSuccess {
+            _result.value = expression.toString()
+        }.onFailure {
+            _uiEffect.value = UiEffect.ShowErrorMessage(it.message)
+        }
     }
 
     fun removeLast() {
-
+        runCatching {
+            expression = expression.removeLast()
+        }.onSuccess {
+            _result.value = expression.toString()
+        }.onFailure {
+            _uiEffect.value = UiEffect.ShowErrorMessage(it.message)
+        }
     }
 
     fun calculate() {
-
+        val result = calculator.calculate(expression.toString())
+        if (result == null) {
+            _uiEffect.value = UiEffect.InCompleteExpressionError
+        } else {
+            expression = Expression(listOf(result))
+            _result.value = result.toString()
+        }
     }
+}
+
+sealed interface UiEffect {
+    object InCompleteExpressionError : UiEffect
+    data class ShowErrorMessage(val message: String?) : UiEffect
 }
