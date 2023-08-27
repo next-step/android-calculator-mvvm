aab900f2c9b1c2a11d5d77d6d04ee2122eaa13db
KwonDae <green201402317@gmail.com>
2023-08-27 21:00:27 +0900

    Test - Expression 초기화 수정된 부분 적용

--- calculator-app/src/test/java/camp/nextstep/edu/calculator/CalculatorViewModelTest.kt
+++ calculator-app/src/test/java/camp/nextstep/edu/calculator/CalculatorViewModelTest.kt
@@ -1,6 +1,7 @@
 package camp.nextstep.edu.calculator
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import camp.nextstep.edu.calculator.domain.Expression
 import camp.nextstep.edu.calculator.domain.Operator
 import com.google.common.truth.Truth.assertThat
 import org.junit.Before
@@ -30,10 +31,9 @@ class CalculatorViewModelTest {
     }
 
     @Test
-    fun `입력된 피연산자가 없을 때, 사용자가 피연산자 버튼을 누르면 해당 숫자가 추가된다2`() {
+    fun `입력된 피연산자가 있을 때, 사용자가 피연산자 버튼을 누르면 해당 숫자가 추가된다2`() {
         // given
-        viewModel.addToExpression(5)
-        viewModel.addToExpression(Operator.Plus)
+        viewModel = CalculatorViewModel(Expression(listOf(5, Operator.Plus)))
 
         // when
         viewModel.addToExpression(3)
@@ -45,7 +45,7 @@ class CalculatorViewModelTest {
     @Test
     fun `입력된 피연산자가 있을 때, 기존 숫자 뒤에 해당 숫자가 화면에 보여야 한다`() {
         // given
-        viewModel.addToExpression(8)
+        viewModel = CalculatorViewModel(Expression(listOf(8)))
 
         // when
         viewModel.addToExpression(9)
@@ -69,7 +69,7 @@ class CalculatorViewModelTest {
     @Test
     fun `입력된 피연산자가 있을 때, 사용자가 연산자 +, -, ×, ÷ 버튼을 누르면 해당 기호가 화면에 보여야 한다`() {
         // given
-        viewModel.addToExpression(8)
+        viewModel = CalculatorViewModel(Expression(listOf(8)))
 
         // when
         viewModel.addToExpression(Operator.Plus)
@@ -81,8 +81,7 @@ class CalculatorViewModelTest {
     @Test
     fun `입력된 피연산자가 있을 때, 사용자가 연산자 +, -, ×, ÷ 버튼을 누르면 해당 기호가 화면에 보여야 한다2`() {
         // given
-        viewModel.addToExpression(8)
-        viewModel.addToExpression(Operator.Plus)
+        viewModel = CalculatorViewModel(Expression(listOf(8, Operator.Plus)))
 
         // when
         viewModel.addToExpression(Operator.Minus)
@@ -103,9 +102,7 @@ class CalculatorViewModelTest {
     @Test
     fun `입력된 수식이 있을 때, 사용자가 지우기 버튼을 누르면 수식에 마지막으로 입력된 연산자 또는 피연산자가 지워져야 한다`() {
         // given
-        viewModel.addToExpression(32)
-        viewModel.addToExpression(Operator.Plus)
-        viewModel.addToExpression(1)
+        viewModel = CalculatorViewModel(Expression(listOf(32, Operator.Plus, 1)))
 
         // when
         viewModel.removeLast()
@@ -118,9 +115,7 @@ class CalculatorViewModelTest {
     @Test
     fun `입력된 수신이 완전할 때, 사용자가 = 버튼을 누르면 입력된 수식의 결과가 화면에 보여야 한다`() {
         // given
-        viewModel.addToExpression(3)
-        viewModel.addToExpression(Operator.Plus)
-        viewModel.addToExpression(2)
+        viewModel = CalculatorViewModel(Expression(listOf(3, Operator.Plus, 2)))
 
         // when
         viewModel.calculate()
@@ -132,8 +127,7 @@ class CalculatorViewModelTest {
     @Test
     fun `입력된 수식이 완전하지 않을 때, 사용자가 = 버튼을 눌렀을 때 완성되지 않은 수식입니다 토스트 메세지가 화면에 보여야 한다`() {
         // given
-        viewModel.addToExpression(3)
-        viewModel.addToExpression(Operator.Plus)
+        viewModel = CalculatorViewModel(Expression(listOf(3, Operator.Plus)))
 
         // when
         viewModel.calculate()
