31d6c31d62b7ed0d48c34a71ee48a6002875db76
KwonDae <green201402317@gmail.com>
2023-08-28 02:03:56 +0900

    Room 인스턴스 생성을 위한 Application 설정

---
+++ calculator-app/src/main/java/camp/nextstep/edu/calculator/CalculatorApplication.kt
@@ -0,0 +1,16 @@
+/**
+ * @author Daewon on 28,August,2023
+ *
+ */
+
+package camp.nextstep.edu.calculator
+
+import android.app.Application
+import camp.nextstep.edu.calculator.data.local.CalculatorDatabase
+import camp.nextstep.edu.calculator.data.repository.CalculatorRepositoryImpl
+
+class CalculatorApplication : Application() {
+
+    val dataBase by lazy { CalculatorDatabase.getDatabase(this) }
+    val repository by lazy { CalculatorRepositoryImpl(dataBase.calculatorDao()) }
+}
