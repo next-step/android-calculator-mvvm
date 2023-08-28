dfda8b8be06d1341f16502b7f4419bbd01ef6337
KwonDae <green201402317@gmail.com>
2023-08-28 16:15:32 +0900

    Data 모듈 test 의존성 추가

--- calculator-data/build.gradle.kts
+++ calculator-data/build.gradle.kts
@@ -39,7 +39,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
@@ -48,4 +47,12 @@ dependencies {
     implementation("androidx.room:room-ktx:2.5.2")
     kapt("androidx.room:room-compiler:2.5.2")
 
+    // test
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.google.truth:truth:1.1.3")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("androidx.test:core-ktx:1.5.0")
+    testImplementation("androidx.room:room-testing:2.5.2")
+    testImplementation("org.robolectric:robolectric:4.9")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 }
